c7ad43cfc0e57f9364e24da45a835705de6400f9
j1c <jaewonc78@gmail.com>
2018-09-16 20:35:59 -0400

    update style

--- graphstats/embed/lpm.py
+++ graphstats/embed/lpm.py
@@ -1,20 +1,21 @@
-
 import numpy as np
 
+
 class LatentPosition:
-	"""
+    """
 	A basic class for a Latent Position Model.
 	"""
-	def __init__(self, X, Y, d=None):
-		self.X = X
-		if np.array_equal(X, Y):
-			self.Y = None
-		else:
-			self.Y = Y
-		self.d = d
 
-	def is_symmetric(self):
-		"""
+    def __init__(self, X, Y, d=None):
+        self.X = X
+        if np.array_equal(X, Y):
+            self.Y = None
+        else:
+            self.Y = Y
+        self.d = d
+
+    def is_symmetric(self):
+        """
 		A function to check whether a latent position model is symmetric.
 		"""
-		return (self.Y is None)
+        return (self.Y is None)
