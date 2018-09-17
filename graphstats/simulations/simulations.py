5a7f668b62a9372d02f9e23d7c55f9ef3337d69a
Eric Bridgeford <ebridge2@jhu.edu>
2018-09-16 23:25:47 -0400

    added sbm code; needs testing

--- graphstats/simulations/simulations.py
+++ graphstats/simulations/simulations.py
@@ -9,7 +9,7 @@ import numpy as np
 from graphstats.utils import import_graph, symmetrize
 
 
-def weighted_sbm(n, P, Wt=1, directed=False, loops=False, Wtargs):
+def weighted_sbm(n, P, Wt=1, directed=False, loops=False, Wtargs=None):
     """
     A function for simulating from a weighted sbm.
 
@@ -20,7 +20,9 @@ def weighted_sbm(n, P, Wt=1, directed=False, loops=False, Wtargs):
             are assigned n[0], n[1], ...
         P: array-like, shape (n_communities, n_communities)
             the probability of an edge between each of the communities,
-            where P[i, j] indicates the 
+            where P[i, j] indicates the probability of a connection
+            between edges in communities [i, j]. 0 < P[i, j] < 1
+            for all i, j.
         directed: boolean
             whether or not the graph will be directed.
         loops: boolean
@@ -34,14 +36,87 @@ def weighted_sbm(n, P, Wt=1, directed=False, loops=False, Wtargs):
             between communities i and j. If the entry is a function, should
             accept an argument for size. An entry of Wt[i, j] = 1 will produce a
             binary subgraph over the i, j community.
-        Wtargs: dictionary or array-like, shape (n_communities, n_communities) 
-            
+        Wtargs: dictionary or array-like, shape (n_communities, n_communities)
+            + if Wt is an object, Wtargs corresponds to the trailing arguments
+            to pass to the weight function.
+            + if Wt is an array-like, Wtargs[i, j] corresponds to trailing
+            arguments to pass to Wt[i, j].            
 
     Returns
     -------
         A: array-like, shape (n, n)
             the adjacency matrix.
     """
+    # type checking
+    if type(n) is not list:
+        raise TypeError("n is not a list.")
+    if not all(type(i) == int for i in n):
+        raise TypeError("An entry of n is not an integer.")
+    if type(directed) is not bool:
+        raise TypeError("directed is not of type bool.")
+    if type(loops) is not bool:
+        raise TypeError("loops is not of type bool.")
+
+    K = len(n)  # the number of communities
+    counter = 0
+    # get a list of community indices
+    for i in range(0, K):
+        cmties.append(range(counter, counter + n[i]))
+        counter += 1
+    if type(P) != np.ndarray:
+        raise TypeError("P is not an ndarray.")
+    if P.shape != (K, K):
+        er_msg = "P is not a square, len(n) x len(n) matrix. P of shape {}"
+        er_msg = er_msg.format(P.shape)
+        raise ValueError(er_msg)
+    if (not directed) and P != P.T:
+        raise ValueError("Specified undirected, but P suggests otherwise.")
+    if type(Wt) == np.ndarray:
+        if Wt.shape != (K, K):
+            er_msg = "Wt is not a square, len(n) x len(n) matrix."
+            er_msg += " Wt is of shape {}".format(Wt.shape)
+            raise ValueError(er_msg)
+        if Wt.shape != Wtargs.shape:
+            er_msg = "Wt is of shape {}, but Wtargs is of shape {}."
+            er_msg += " They should have the same shape."
+            raise ValueError(er_msg) 
+    else:
+        # reshape to make an ndarray for each community
+        Wt = np.full(P.shape, Wt, dtype=object)
+        Wtargs = np.full(P.shape, Wtargs, dtype=object)
+    # check probabilities are valid
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError("Your probability matrix is not 0 < P[i, j] < 1.")
+
+    A = np.zeros((sum(n), sum(n)))
+
+    for i in range(0, K):
+        if directed:
+            jrange = range(0, K)
+        else:
+            jrange = range(i, K)
+        for j in jrange:
+            wt = Wt[i, j]; wtargs = Wtargs[i, j]; p = P[i, j]
+            # identify submatrix for community i, j
+            smtx = np.zeros((sum(n), sum(n)))
+            smtx[cmties[i], cmties[j]] = True
+            idx = np.where(smtx)
+            # get idx in 1d coordinates by ravelling
+            triu = np.ravel_multi_index(idx, dims=A.shape)
+            # choose M of them
+            triu = np.random.choice(triu, size=M, replace=False)
+            # unravel back
+            triu = np.unravel_index(triu, dims=A.shape)
+            if type(wt) is not int:
+                if not callable(wt):
+                    raise TypeError("You have not passed a function for wt.")
+                wt = wt(size=len(triu), **wtargs)
+            A[triu] = wt
+    if not loops:
+        A = A - np.diag(np.diag(A))
+    if not directed:
+        A = symmetrize(A)
+    return(A)
 
 
 def zi_nm(n, M, wt=1, directed=False, loops=False, **kwargs):
@@ -78,6 +153,10 @@ def zi_nm(n, M, wt=1, directed=False, loops=False, **kwargs):
         raise TypeError("M is not of type int.")
     if type(n) is not int:
         raise TypeError("n is not of type int.")
+    if type(directed) is not bool:
+        raise TypeError("directed is not of type bool.")
+    if type(loops) is not bool:
+        raise TypeError("loops is not of type bool.")
     # check for loopiness
     if loops:
         er_msg = "n^2"
@@ -114,7 +193,7 @@ def zi_nm(n, M, wt=1, directed=False, loops=False, **kwargs):
         raise ValueError(msg)
 
     # check weight function
-    if wt != 1:
+    if type(wt) is not int:
         if not callable(wt):
             raise TypeError("You have not passed a function for wt.")
         # optionally, consider weighted model
@@ -167,6 +246,10 @@ def zi_np(n, p, wt=1, directed=False, loops=False, **kwargs):
         raise TypeError("p is not of type float.")
     if type(n) is not int:
         raise TypeError("n is not of type int.")
+    if type(directed) is not bool:
+        raise TypeError("directed is not of type bool.")
+    if type(loops) is not bool:
+        raise TypeError("loops is not of type bool.")
     # check p
     if p < 0:
         msg = "You have passed a probability, {}, less than 0."
@@ -209,7 +292,7 @@ def zi_np(n, p, wt=1, directed=False, loops=False, **kwargs):
     # connected with probability p
     triu = triu[pchoice < p]
 
-    if wt != 1:
+    if type(wt) is not int:
         if not callable(wt):
             raise TypeError("You have not passed a function for wt.")
         # optionally, consider weighted model
