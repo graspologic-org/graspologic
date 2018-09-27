85d44bcb48183b1ae309a2b385e76162b5171e8a
bdpedigo <benjamindpedigo@gmail.com>
2018-09-27 15:56:28 -0400

    added LSE, restructured tests

--- graphstats/embed/__init__.py
+++ graphstats/embed/__init__.py
@@ -1,5 +1,5 @@
 from .omni import OmnibusEmbed
 from .ase import AdjacencySpectralEmbed
+from .lse import LaplacianSpectralEmbed
 
-__all__ = [OmnibusEmbed, AdjacencySpectralEmbed]
-
+__all__ = [OmnibusEmbed, AdjacencySpectralEmbed, LaplacianSpectralEmbed]
