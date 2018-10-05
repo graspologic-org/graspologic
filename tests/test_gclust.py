19574c5ce19cd9c55013c9c004ac10908d6d7f9f
j1c <jaewonc78@gmail.com>
2018-10-05 17:26:24 -0400

    Include more tests

--- tests/test_gclust.py
+++ tests/test_gclust.py
@@ -1,8 +1,8 @@
 import pytest
 import numpy as np
-from numpy import array_equal, allclose
+from numpy.testing import assert_allclose, assert_equal
 
-from graspy.cluster import GaussianCluster
+from graspy.cluster.gclust import GaussianCluster
 
 
 def test_inputs():
@@ -14,7 +14,35 @@ def test_inputs():
         gclust = GaussianCluster(100)
         gclust.fit(X)
 
+    with pytest.raises(ValueError):
+        gclust = GaussianCluster(100)
+        gclust.fit_predict(X)
+
     # max_cluster < 0
     with pytest.raises(ValueError):
         gclust = GaussianCluster(max_components=-1)
         gclust.fit(X)
+
+    with pytest.raises(ValueError):
+        gclust = GaussianCluster(max_components=-1)
+        gclust.fit_predict(X)
+
+
+def test_outputs():
+    np.random.seed(1)
+
+    n = 100
+    d = 3
+
+    X1 = np.random.normal(2, 1, size=(n, d))
+    X2 = np.random.normal(-2, 1, size=(n, d))
+    X = np.vstack((X1, X2))
+    y = np.repeat([0, 1], n)
+
+    gclust = GaussianCluster(max_components=3)
+    gclust.fit(X, y)
+    bics = gclust.bic_
+    aris = gclust.ari_
+
+    assert_equal(np.argmin(bics), 1)
+    assert_allclose(aris[1], 1)
--- tests/test_mds.py
+++ tests/test_mds.py
@@ -1,12 +1,28 @@
+import pytest
 import numpy as np
 from numpy.testing import assert_almost_equal
 
 from graspy.embed.mds import ClassicalMDS
 
 
+def test_input():
+    X = np.random.normal(0, 1, size=(10, 3))
+
+    # n_components > n_samples
+    with pytest.raises(ValueError):
+        mds = ClassicalMDS(n_components=100)
+        mds.fit(X)
+
+    with pytest.raises(ValueError):
+        mds = ClassicalMDS(n_components=2)
+        mds.fit(X)
+
+
 def test_output():
     """
     Recover a 3D tetrahedron with distance 1 between all points
+
+    Use both fit and fit_transform functions
     """
 
     def _compute_dissimilarity(arr):
@@ -17,12 +33,28 @@ def test_output():
 
         return out
 
-    A = np.ones((4, 4)) - np.identity(4)
+    def use_fit_transform():
+        A = np.ones((4, 4)) - np.identity(4)
 
-    mds = ClassicalMDS(n_components=3)
-    B = mds.fit_transform(A)
+        mds = ClassicalMDS(n_components=3)
+        B = mds.fit_transform(A)
 
-    Ahat = _compute_dissimilarity(B)
+        Ahat = _compute_dissimilarity(B)
 
-    # Checks up to 7 decimal points
-    assert_almost_equal(A, Ahat)
+        # Checks up to 7 decimal points
+        assert_almost_equal(A, Ahat)
+
+    def use_fit():
+        A = np.ones((4, 4)) - np.identity(4)
+
+        mds = ClassicalMDS(n_components=3)
+        mds.fit(A)
+        B = np.dot(mds.components_, np.diag(mds.singular_values_))
+
+        Ahat = _compute_dissimilarity(B)
+
+        # Checks up to 7 decimal points
+        assert_almost_equal(A, Ahat)
+
+    use_fit_transform()
+    use_fit()
