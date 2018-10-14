4c40374d993c8684af9820fe66071b78a1a665d8
bdpedigo <benjamindpedigo@gmail.com>
2018-10-14 12:38:34 -0400

    semipar exists

--- graspy/inference/semipar.py
+++ graspy/inference/semipar.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .base import BaseInference
 from ..embed import AdjacencySpectralEmbed, LaplacianSpectralEmbed, OmnibusEmbed
-from ..simulations import er_nm
+from ..simulations import rdpg
 from scipy.spatial import procrustes
 
 class SemiparametricTest(BaseInference):
@@ -29,21 +29,20 @@ class SemiparametricTest(BaseInference):
         dimensions are found by the Zhu and Godsi algorithm.
     """
 
-    def __init__(self, embedding='ase', n_components=2, n_bootstraps=100, *args, **kwargs):
+    def __init__(self, embedding='ase', n_components=2, n_bootstraps=1000, *args, **kwargs):
         super().__init__(embedding=embedding, n_components=n_components, *args, **kwargs)
         self.n_bootstraps = n_bootstraps
  
     def _bootstrap(self, X_hat):
         t_bootstrap = np.zeros((self.n_bootstraps))
         for i in range(self.n_bootstraps):
-            X1_hat_simulated = er_nm(X_hat.shape[0],2) # TODO: replace with RDPG sampled from X_hat
-            X2_hat_simulated = er_nm(X_hat.shape[0],2)
+            X1_hat_simulated = rdpg(X_hat) 
+            X2_hat_simulated = rdpg(X_hat)
             f_norm = procrustes(X1_hat_simulated, X2_hat_simulated)[2] # TODO: swap out procrustes()[2] with other forms 
                                                                        # to test orthogonal case and arbitrary diagonal case
             t_bootstrap[i] = f_norm
-
         
-        return 
+        return t_bootstrap
 
     def _embed(self, A1, A2):
         if self.embedding not in ['ase', 'lse', 'omnibus']: 
@@ -55,13 +54,13 @@ class SemiparametricTest(BaseInference):
         X1_hat = np.array([]) 
         X2_hat = np.array([])
         if self.embedding == 'ase':
-            X1_hat = AdjacencySpectralEmbed(k=self.n_components).fit_transform(A1)
-            X2_hat = AdjacencySpectralEmbed(k=self.n_components).fit_transform(A2)
+            X1_hat = AdjacencySpectralEmbed(k=self.n_components).fit(A1).X
+            X2_hat = AdjacencySpectralEmbed(k=self.n_components).fit(A2).X
         elif self.embedding == 'lse':
-            X1_hat = LaplacianSpectralEmbed(k=self.n_components).fit_transform(A1)
-            X2_hat = LaplacianSpectralEmbed(k=self.n_components).fit_transform(A2)
+            X1_hat = LaplacianSpectralEmbed(k=self.n_components).fit(A1).X
+            X2_hat = LaplacianSpectralEmbed(k=self.n_components).fit(A2).X
         elif self.embedding == 'omnibus':
-            X_hat_compound = OmnibusEmbed(k=self.n_components).fit_transform([A1, A2])
+            X_hat_compound = OmnibusEmbed(k=self.n_components).fit([A1, A2]).X
             X1_hat = X_hat_compound[:A1.shape[0],:]
             X2_hat = X_hat_compound[A2.shape[1]:,:]
         
@@ -69,8 +68,17 @@ class SemiparametricTest(BaseInference):
 
     def fit(self, A1, A2):
         X_hats = self._embed(A1, A2)
-        self._bootstrap(X_hats)
 
+        # TODO : what to store as fields here 
+        T1_bootstrap = self._bootstrap(X_hats[0])
+        T2_bootstrap = self._bootstrap(X_hats[1])
 
+        T_sample = procrustes(X_hats[0], X_hats[1])[2] # TODO: swap out procrustes()[2] with other forms 
+                                                       # to test orthogonal case and arbitrary diagonal case
 
-    
+        p1 = len(T1_bootstrap[T1_bootstrap > T_sample] + 0.5) / self.n_bootstraps
+        p2 = len(T2_bootstrap[T2_bootstrap > T_sample] + 0.5) / self.n_bootstraps
+
+        p = max(p1, p2)
+
+        return p
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -1,37 +1,19 @@
 import unittest
 import numpy as np
 from graspy.inference import SemiparametricTest
+from graspy.embed import AdjacencySpectralEmbed
 
 class TestSemiparametricTest(unittest.TestCase):
 
-    def test_private_embed(self):
-        '''
-            throwaway, temporary test
-        '''
+    def test_fit_p(self):
         A1 = np.array([[0, 1, 0],
                        [1, 0, 1],
                        [0, 1, 0]])
 
-        A2 = np.array([[1, 1, 1],
-                       [1, 1, 1],
-                       [1, 1, 1]])
-
-        spt = SemiparametricTest()
-        X1_hat, X2_hat = spt._embed(A1, A2)
-
-    def test_private_bootstraps(self):
-        '''
-            throwaway, temporary test
-        '''
-        A1 = np.array([[0, 1, 0],
-                       [1, 0, 1],
+        A2 = np.array([[0, 1, 0],
+                       [1 ,0, 1],
                        [0, 1, 0]])
 
-        A2 = np.array([[1000, 1, 0],
-                       [1 ,1, 1],
-                       [0, 1, 1000]])
-
         spt = SemiparametricTest()
-        X_hats = spt._embed(A1, A2)
-        ts = spt._bootstrap(X_hats)
-        print(ts)
+        p = spt.fit(A1, A2)
+        print(p)
