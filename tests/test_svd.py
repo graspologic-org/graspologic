727c2ab0f9d711266698bd6f907da19b705837b6
bvarjavand <bvarjavand@gmail.com>
2018-10-14 22:41:55 -0400

    fixed test, added notebook

--- tests/test_svd.py
+++ tests/test_svd.py
@@ -8,7 +8,6 @@ from numpy.linalg import norm
 from graphstats.embed.svd import selectDim
 from scipy.linalg import orth
 
-
 def generate_data(n=10, elbows=3, seed=1):
     """
     Generate data matrix with a specific number of elbows on scree plot
@@ -17,15 +16,13 @@ def generate_data(n=10, elbows=3, seed=1):
     x = np.random.binomial(1,.6,(n**2)).reshape(n,n)
     xorth = orth(x)
     d = np.zeros(xorth.shape[0])
-    #for i in range(0,len(d), int(len(d)/(elbows+1))):
-    d[:5] += 10
+    for i in range(0,len(d), int(len(d)/(elbows+1))):
+        d[:i] += 10
     A = xorth.T @ np.diag(d) @ xorth
     return A,d
 
 def test_zg_1_elbow():
     # Should get all ones
-    data,l = generate_data(10,1)
+    data,l = generate_data(10,3)
     elbows, likelihoods, sing_vals, all_likelihoods = selectDim(data, 3)
-    assert elbows[0]==6
-
- 
+    assert elbows[0]==5
