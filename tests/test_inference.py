9d37cbb24766053c915f7b2a7b493abfb6542a9f
bdpedigo <benjamindpedigo@gmail.com>
2018-10-18 19:24:45 -0400

    adding more typechecking

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -4,26 +4,27 @@ from graspy.inference import SemiparametricTest
 from graspy.embed import AdjacencySpectralEmbed
 
 class TestSemiparametricTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.A1 = np.array([[0, 1, 0],
+                           [1, 0, 1],
+                           [0, 1, 0]])
+
+        cls.A2 = np.array([[0, 1, 0],
+                           [1 ,0, 1],
+                           [0, 1, 0]])
 
     def test_fit_p(self):
-        A1 = np.array([[0, 1, 0],
-                       [1, 0, 1],
-                       [0, 1, 0]])
-
-        A2 = np.array([[0, 1, 0],
-                       [1 ,0, 1],
-                       [0, 1, 0]])
-
         spt = SemiparametricTest()
-        p = spt.fit(A1, A2)
+        p = spt.fit(self.A1, self.A2)
 
-    def test_input_checking(self):
-        with self.assertRaises(ValueError):
-            spt = SemiparametricTest(n_components=-100)
+    def test_bad_kwargs(self):
         with self.assertRaises(ValueError):
             SemiparametricTest(n_components=-100)
         with self.assertRaises(ValueError):
-            spt = SemiparametricTest(test_case='oops')
+            SemiparametricTest(n_components=-100)
+        with self.assertRaises(ValueError):
+            SemiparametricTest(test_case='oops')
         with self.assertRaises(ValueError):
             SemiparametricTest(n_bootstraps=-100)
         with self.assertRaises(ValueError):
@@ -36,6 +37,14 @@ class TestSemiparametricTest(unittest.TestCase):
             SemiparametricTest(embedding=6)
         with self.assertRaises(TypeError):
             SemiparametricTest(test_case=6)
+
+    def test_n_bootstraps(self):
+        spt = SemiparametricTest(n_bootstraps=234)
+        spt.fit(self.A1, self.A2)
+        self.assertEqual(spt.T1_bootstrap.shape[0], 234)
+    
+    
+
 
 if __name__ == '__main__':
     unittest.main()
