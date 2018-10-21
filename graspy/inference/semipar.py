9b51d4d224361e830a7b5096f6c98e12907562cd
bvarjavand <bvarjavand@gmail.com>
2018-10-21 16:09:40 -0400

    added scaling and diagonal rotation to semipar

--- graspy/inference/semipar.py
+++ graspy/inference/semipar.py
@@ -1,4 +1,4 @@
-# Ben Pedigo 
+# Ben Pedigo
 # bpedigo [at] jhu.edu
 # 10.18.2018
 
@@ -29,7 +29,7 @@ class SemiparametricTest(BaseInference):
             and use Procrustes to align the embeddings.
         'omnibus'
             Embed all graphs simultaneously using omnibus embedding.
-    
+
     n_components : None (default), or Int
         Number of embedding dimensions. If None, the optimal embedding
         dimensions are found by the Zhu and Godsi algorithm.
@@ -38,30 +38,30 @@ class SemiparametricTest(BaseInference):
     """
 
     def __init__(self, embedding='ase', n_components=2, n_bootstraps=1000, test_case='rotation',):
-        
+
         if type(n_bootstraps) is not int:
             raise TypeError()
         if type(test_case) is not str:
             raise TypeError()
-        
+
         if n_bootstraps < 1:
             raise ValueError('{} is invalid number of bootstraps, must be greater than 1'.format(n_bootstraps))
         if test_case not in ['rotation', 'scaling-rotation', 'diagonal-rotation']:
             raise ValueError('test_case must be one of \'rotation\', \'scaling-rotation\',\'diagonal-rotation\'')
 
         super().__init__(embedding=embedding, n_components=n_components,)
-        
+
         self.n_bootstraps = n_bootstraps
         self.test_case = test_case
- 
+
     def _bootstrap(self, X_hat):
         t_bootstrap = np.zeros((self.n_bootstraps))
         for i in range(self.n_bootstraps):
-            A1_simulated = rdpg(X_hat) 
+            A1_simulated = rdpg(X_hat)
             A2_simulated = rdpg(X_hat)
             X1_hat_simulated, X2_hat_simulated = self._embed(A1_simulated, A2_simulated)
             t_bootstrap[i] = self._norm(X1_hat_simulated, X2_hat_simulated)
-        
+
         return t_bootstrap
 
     def _norm(self, X1, X2):
@@ -70,18 +70,41 @@ class SemiparametricTest(BaseInference):
                 R = orthogonal_procrustes(X1, X2)[0]
                 return np.linalg.norm(np.dot(X1, R) - X2)
             elif self.test_case == 'scalar-rotation':
-                raise NotImplementedError() # TODO
+                X1 -= np.mean(X1, 0)
+                X2 -= np.mean(X2, 0)
+                R = orthogonal_procrustes(X1, X2)[0]
+                return np.linalg.norm(np.dot(X1,R) - X2)
             elif self.test_case == 'diagonal-rotation':
-                raise NotImplementedError() # TODO
+                normX1 = np.linalg.norm(X1)
+                normX2 =  np.linalg.norm(X2)
+                X1 /= normX1
+                X2 /= normX2
+                R,s = orthogonal_procrustes(X1, X2)
+                X2 = np.dot(X2, R.T) * s
+                return np.linalg.norm(X1 - X2)
+            elif self.test_case == 'scalar-diagonal-rotation':
+                '''
+                X1 = X1-np.mean(X1, 0)
+                X2 = X1-np.mean(X2, 0)
+                normX1 = np.linalg.norm(X1)
+                normX2 =  np.linalg.norm(X2)
+                X1 /= normX1
+                X2 /= normX2
+                R,s = orthogonal_procrustes(X1, X2)
+                X2 = np.dot(X2, R.T)*s
+                return np.linalg.norm(X1 - X2)
+                '''
+                mx1, mx2, disparity = procrustes(X1,X2)
+                return disparity
         else:
             return np.linalg.norm(X1 - X2)
 
     def _embed(self, A1, A2):
-        if self.embedding not in ['ase', 'lse', 'omnibus']: 
+        if self.embedding not in ['ase', 'lse', 'omnibus']:
             raise ValueError('Invalid embedding method "{}"'.format(self.embedding))
-        
+
         if self.n_components is None:
-            raise NotImplementedError('Wait for dimselect') # TODO 
+            raise NotImplementedError('Wait for dimselect') # TODO
 
         if self.embedding == 'ase':
             X1_hat = AdjacencySpectralEmbed(k=self.n_components).fit_transform(A1)
@@ -93,7 +116,7 @@ class SemiparametricTest(BaseInference):
             X_hat_compound = OmnibusEmbed(k=self.n_components).fit_transform((A1, A2))
             X1_hat = X_hat_compound[:A1.shape[0],:]
             X2_hat = X_hat_compound[A2.shape[1]:,:]
-        
+
         return (X1_hat, X2_hat)
 
     def fit(self, A1, A2):
@@ -102,12 +125,12 @@ class SemiparametricTest(BaseInference):
 
         if not is_symmetric(A1) or not is_symmetric(A2):
             raise NotImplementedError() # TODO asymmetric case
-        
+
         if A1.shape != A2.shape:
             raise ValueError('Input matrices do not have matching dimensions')
 
         X_hats = self._embed(A1, A2)
-        
+
         T1_bootstrap = self._bootstrap(X_hats[0])
         T2_bootstrap = self._bootstrap(X_hats[1])
 
@@ -117,10 +140,10 @@ class SemiparametricTest(BaseInference):
         p2 = (len(T2_bootstrap[T2_bootstrap >= T_sample]) + 0.5) / self.n_bootstraps
 
         p = max(p1, p2)
-        
+
         # TODO : what to store as fields here, or make _private fields
         # at least for the sake of testing, I'm going to keep everything
-        
+
         self.T1_bootstrap = T1_bootstrap
         self.T2_bootstrap = T2_bootstrap
         self.T_sample = T_sample
