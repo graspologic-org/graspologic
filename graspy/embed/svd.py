0702914a04fb308cc2c530a5e40bdc6d92776877
Bijan Varjavand <bvarjavand@gmail.com>
2018-10-25 13:33:15 -0400

    notebooks + zg (#50)
    * notebooks + zg
    
    * Remove unnecessary code
    
    * Update zg test imports
    
    * Change @ to dot
    
    * Update selectDim function
    
    * Update function and change name to select_dimension
    
    * Include more tests
    
    * Remove unnecessary lines
    
    * Update selectSVD
    
    * Rename and include SBM test
    
    * Fix test error. Oops
    
    * Include n_elbows option in selectSVD

--- graspy/embed/svd.py
+++ graspy/embed/svd.py
@@ -7,70 +7,181 @@
 
 from sklearn.decomposition import TruncatedSVD
 from scipy.sparse.linalg import svds
+import numpy as np
+from scipy.stats import norm
 
 
-def selectDim(X, method=TruncatedSVD, *args, **kwargs):
+def _compute_likelihood(arr):
     """
-    A function that uses likelihood profiling to determine the optimal 
-    number of embedding dimensions.
+    Computes the log likelihoods based on normal distribution given 
+    a 1d-array of sorted values.
+    """
+    n_elements = len(arr)
+    likelihoods = np.zeros(n_elements)
+
+    for idx in range(1, n_elements + 1):
+        # split into two samples
+        s1 = arr[:idx]
+        s2 = arr[idx:]
+        if s2.size == 0:  # deal with when idx == n_elements
+            s2 = np.zeros(1)
+
+        # compute means
+        mu1 = np.mean(s1)
+        mu2 = np.mean(s2)
+
+        # compute pooled variance
+        variance = ((np.sum((s1 - mu1)**2) + np.sum(
+            (s2 - mu2)**2))) / (n_elements - 2)
+        std = np.sqrt(variance)
+
+        # compute log likelihoods
+        likelihoods[idx - 1] = np.sum(norm.logpdf(
+            s1, loc=mu1, scale=std)) + np.sum(
+                norm.logpdf(s2, loc=mu2, scale=std))
+
+    return likelihoods
+
+
+def select_dimension(X,
+                     n_components=None,
+                     n_elbows=2,
+                     threshold=None,
+                     return_likelihoods=False):
+    """
+    Generates profile likelihood from array based on Zhu and Godsie method.
+    Elbows correspond to the optimal embedding dimension.
 
     Parameters
     ----------
-        X: {array-like}, shape (n_samples, n_features)
-         The input data to select the optimal embedding dimensionality for.
-        method: object (default TruncatedSVD)
-        args: list, optional (default None)
-         options taken by the desired embedding method as arguments.
-        kwargs: dict, optional (default None)
-         options taken by the desired embedding method as key-worded
-         arguments.
+    X : 1d or 2d array-like
+        Input array generate profile likelihoods for. If 1d-array, it should be
+        sorted in decreasing order. If 2d-array, shape should be 
+        (n_samples, n_features). 
+    n_components : int, optional, default: None.
+        Number of components to embed. If None, ``n_components = 
+        floor(log2(min(n_samples, n_features)))``. Ignored if X is 1d-array.
+    n_elbows : int, optional, default: 2.
+        Number of likelihood elbows to return. Must be > 1. 
+    threshold : float, int, optional, default: None
+        If given, only consider the singular values that are > threshold. Must
+        be >= 0.
+    return_likelihoods : bool, optional, default: False
+        If True, returns the all likelihoods associated with each elbow. 
 
     Returns
     -------
-    A dictionary containing the following:
-        optimal_d: {int}
-         the optmial number of embedding dimensions.
-        optimal_lq: {float}
-         the likelihood of the optimal number of embedding dimensions.
-        ds: {array-like}, shape (n_components)
-         the singular values associated with the decomposition of X,
-         from which optimal_d was chosen.
-        lqs: {array-like}, shape (n_components)
-         the likelihood profile for all possible embedding dimensions
-         ds.
+    elbows : list
+        Elbows indicate subsequent optimal embedding dimensions. Number of
+        elbows may be less than n_elbows if there are not enough singular 
+        values.
+    sing_vals : list
+        The singular values associated with each elbow.
+    likelihoods : list of array-like
+        Array of likelihoods of the corresponding to each elbow. Only returned
+        if `return_likelihoods` is True.
 
-    See Also
-    --------
-        TruncatedSVD
+    References
+    ----------
+    .. [1] Zhu, M. and Ghodsi, A. (2006).
+        Automatic dimensionality selection from the scree plot via the use of
+        profile likelihood. Computational Statistics & Data Analysis, 51(2), 
+        pp.918-930.
 
-    References:
-    -----------
-    Automatic dimensionality selection from the scree plot via the use of 
-    profile likelihood
-    Zhu, Mu and Ghodsi, Ali. CSDA 2006. 
-    https://www.sciencedirect.com/science/article/pii/S0167947305002343
     """
-    raise NotImplementedError()
+    # Handle input data
+    if not isinstance(X, np.ndarray):
+        msg = 'X must be a numpy array, not {}.'.format(type(X))
+        raise ValueError(msg)
+    if X.ndim > 2:
+        msg = 'X must be a 1d or 2d-array, not {}d array.'.format(X.ndim)
+        raise ValueError(msg)
+    elif np.min(X.shape) == 1:
+        msg = 'X is 2d-array and must have more than 1 samples or 1 features.'
+        raise ValueError(msg)
 
-    return {
-        'optimal_d': optimal_d,
-        'optimal_lq': optimal_lq,
-        'ds': ds,
-        'lqs': lqs
-    }
+    # Handle n_elbows
+    if not isinstance(n_elbows, int):
+        msg = 'n_elbows must be an integer, not {}.'.format(type(n_elbows))
+        raise ValueError(msg)
+    elif n_elbows < 1:
+        msg = 'number of elbows should be an integer > 1, not {}.'.format(
+            n_elbows)
+        raise ValueError(msg)
+
+    # Handle threshold
+    if threshold is not None:
+        if not isinstance(threshold, (int, float)):
+            msg = 'threshold must be an integer or a float, not {}.'.format(
+                type(threshold))
+            raise ValueError(msg)
+        elif threshold < 0:
+            msg = 'threshold must be >= 0, not {}.'.format(threshold)
+            raise ValueError(msg)
+
+    # Handle n_components
+    if n_components is None:
+        if np.min(X.shape) == 1:
+            k = 1
+        else:  # per recommendation by Zhu & Godsie
+            k = np.floor(np.log2(np.min(X.shape)))
+    elif not isinstance(n_components, int):
+        msg = 'n_components must be an integer, not {}.'.format(
+            type(n_components))
+        raise ValueError(msg)
+    else:
+        k = n_components
+
+    # Check to see if svd is needed
+    if X.ndim == 1:
+        D = np.sort(X)[::-1]
+    elif X.ndim == 2:
+        # Singular values in decreasing order
+        D = svds(A=X, k=k, return_singular_vectors=False)[::-1]
+
+    if threshold is not None:
+        D = D[D > threshold]
+
+    if len(D) == 0:
+        msg = 'No values greater than threshold {}.'
+        raise IndexError(msg.format(threshold))
+    elif len(D) <= n_elbows:
+        msg = 'n_elbows must between {}, the number of thresholded \
+        singular values'.format(len(D))
+
+    idx = 0
+    elbows = []
+    values = []
+    likelihoods = []
+    for _ in range(n_elbows):
+        arr = D[idx:]
+        if arr.size <= 2:  # Cant compute likelihoods with 2 numbers
+            break
+        lq = _compute_likelihood(arr)
+        idx += (np.argmax(lq) + 1)
+        elbows.append(idx)
+        values.append(idx - 1)
+        likelihoods.append(lq)
+
+    if return_likelihoods:
+        return elbows, values, likelihoods
+    else:
+        return elbows, values
 
 
-def selectSVD(X, k=None):
+def selectSVD(X, k=None, n_elbows=2):
     """
     A function for performing svd using ZG2, X = U S Vt.
 
     Parameters
     ----------
     X: array-like, shape (n_samples, n_features)
-        the data to perform svd on.
+        The data to perform svd on.
     k: int
-        the number of dimensions to embed into. Should have
-        k < min(X.shape).
+        The number of dimensions to embed into. Should have k < min(X.shape).
+    n_elbows: int, optional, default: 2
+        If `k=None`, then compute the optimal embedding dimension using
+        `select_dimension`. `k=elbows[-1]`.
 
     Returns
     -------
@@ -82,8 +193,12 @@ def selectSVD(X, k=None):
         the singular values, as a 1d array.
     """
     if (k is None):
-        selectDim(X)
-    if k > min(X.shape):
+        elbows, _ = select_dimension(X, n_elbows=n_elbows, threshold=None)
+        k = elbows[-1]
+    if k > min(
+            X.shape
+    ):  #TODO this method does not properly catch error if k=min(X.shape),
+        # also may be unecessary (see svds error catching)
         msg = "k is {}, but min(X.shape) is {}."
         msg = msg.format(k, min(X.shape))
         raise ValueError(msg)
---
+++ tests/test_select_dimension.py
@@ -0,0 +1,108 @@
+import numpy as np
+import pytest
+from numpy.testing import assert_equal
+from scipy.linalg import orth
+
+from graspy.embed.svd import select_dimension
+from graspy.simulations.simulations import binary_sbm
+
+
+def generate_data(n=10, elbows=3, seed=1):
+    """
+    Generate data matrix with a specific number of elbows on scree plot
+    """
+    np.random.seed(seed)
+    x = np.random.binomial(1, .6, (n**2)).reshape(n, n)
+    xorth = orth(x)
+    d = np.zeros(xorth.shape[0])
+    for i in range(0, len(d), int(len(d) / (elbows + 1))):
+        d[:i] += 10
+    A = xorth.T.dot(np.diag(d)).dot(xorth)
+    return A, d
+
+
+def test_invalid_inputes():
+    X, D = generate_data()
+
+    # invalid n_elbows
+    with pytest.raises(ValueError):
+        bad_n_elbows = -2
+        select_dimension(X, threshold=bad_n_elbows)
+
+    with pytest.raises(ValueError):
+        bad_n_elbows = "string"
+        select_dimension(X, threshold=bad_n_elbows)
+
+    with pytest.raises(ValueError):
+        bad_n_elbows = [123]
+        select_dimension(X, threshold=bad_n_elbows)
+
+    # invalid n_components
+    with pytest.raises(ValueError):
+        bad_n_components = -1
+        select_dimension(X, n_components=bad_n_components)
+
+    with pytest.raises(ValueError):
+        bad_n_components = "string"
+        select_dimension(X, n_components=bad_n_components)
+
+    # invalid threshold
+    with pytest.raises(ValueError):
+        bad_threshold = -2
+        select_dimension(X, threshold=bad_threshold)
+
+    with pytest.raises(ValueError):
+        bad_threshold = "string"
+        select_dimension(X, threshold=bad_threshold)
+
+    # invalid X
+    with pytest.raises(ValueError):
+        bad_X = -2
+        select_dimension(X=bad_X)
+
+    with pytest.raises(ValueError):
+        # input is tensor
+        bad_X = np.random.normal(size=(100, 10, 10))
+        select_dimension(X=bad_X)
+
+    with pytest.raises(ValueError):
+        bad_X = np.random.normal(size=100).reshape(100, -1)
+        select_dimension(X=bad_X)
+
+
+def test_output_synthetic():
+    data, l = generate_data(10, 3)
+    elbows, _ = select_dimension(X=data, n_components=9, n_elbows=3)
+    assert_equal(elbows, [1, 5, 7])
+
+
+def test_output_simple():
+    """
+    Elbow should be at 2.
+    """
+    X = np.array([10, 9, 3, 2, 1])
+    elbows, _ = select_dimension(X, n_elbows=1)
+    assert_equal(elbows[0], 2)
+
+
+def test_output_uniform():
+    """
+    Generate two sets of synthetic eigenvalues based on two uniform distributions.
+    The elbow must be at 50.
+    """
+    np.random.seed(9)
+    x1 = np.random.uniform(0, 45, 50)
+    x2 = np.random.uniform(55, 100, 50)
+    X = np.sort(np.hstack([x1, x2]))[::-1]
+    elbows, _ = select_dimension(X, n_elbows=1)
+    assert_equal(elbows[0], 50)
+
+
+def test_output_two_block_sbm():
+    np.random.seed(10)
+    n_communities = [100, 100]
+    P = np.array([[0.5, 0.1], [0.1, 0.5]])
+    A = binary_sbm(n_communities, P)
+
+    elbows, _ = select_dimension(A, n_elbows=2)
+    assert_equal(elbows[0], 2)
