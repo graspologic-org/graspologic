d251f411f70db9347c505733e441606a4e437f3e
j1c <jaewonc78@gmail.com>
2018-11-19 18:25:27 -0500

    Include canonical url

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ release = '2'
 # needs_sphinx = '1.0'
 
 # Environment variable to know if the docs are being built on rtd.
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -92,10 +92,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-if not on_rtd:
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -104,6 +103,7 @@ if not on_rtd:
 html_theme_options = {
     #'includehidden': False,
     'navigation_depth': 2,
+    'canonical_url': 'graspy.neurodata.io',
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
