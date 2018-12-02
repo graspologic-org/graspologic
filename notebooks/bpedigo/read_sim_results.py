fd6e54214900ba57fbb6f9d2382ec3122501947d
bdpedigo <benjamindpedigo@gmail.com>
2018-12-02 12:43:27 -0500

    sim reading

--- notebooks/bpedigo/read_sim_results.py
+++ notebooks/bpedigo/read_sim_results.py
@@ -13,7 +13,18 @@ from multiprocessing import Pool, cpu_count
 # f = open('2110072216651244771.pickle', 'rb')
 # f = open('931210899241672712.pickle', 'rb')
 # f = open('476622349264429854.pickle', 'rb')
-f = open('611522530389271978.pickle', 'rb')
+f = open('1997605371270819926.pickle', 'rb')
 d = pickle.load(f)
 f.close()
-print(d)
+print(d.keys())
+print(d['test_by_epsilon'][0][0])
+tests_by_epsilon = d['test_by_epsilon']
+test_null_p = np.zeros((1000,4))
+test_alt_p = np.zeros((1000,4))
+for e, test_epsilon in enumerate(tests_by_epsilon):
+    for t, test_group in enumerate(test_epsilon):
+        test_null_p[t,e] = test_group[0].p
+        test_alt_p[t,e] = test_group[1].p
+print(np.mean(test_null_p, axis=0))
+print(np.mean(test_alt_p, axis=0))
+    
