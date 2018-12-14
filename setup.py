ec4e6d84d61e90efcee531b082ac7bf9b99f1eb8
Benjamin Pedigo <benjamindpedigo@gmail.com>
2018-12-14 11:35:06 -0500

    Removed python 3.4 from classifiers
    so it doesn't show up on pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.md', 'r') as f:
 AUTHOR = 'Eric Bridgeford, Jaewon Chung, Benjamin Pedigo, Bijan Varjavand',
 AUTHOR_EMAIL = 'ebridge2@jhu.edu'
 URL = 'https://github.com/neurodata/graspy'
-MINIMUM_PYTHON_VERSION = 3, 5  # Minimum of Python 3.4
+MINIMUM_PYTHON_VERSION = 3, 5  # Minimum of Python 3.5
 REQUIRED_PACKAGES = [
     'networkx>=2.1',
     'numpy>=1.8.1',
@@ -47,7 +47,6 @@ setup(
         'Topic :: Scientific/Engineering :: Mathematics',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
