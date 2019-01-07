69c2ea427daf7e8b5927aea870e19aa4bb0b2432
j1c <jaewonc78@gmail.com>
2019-01-07 12:36:23 -0500

    Include inference import

--- graspy/__init__.py
+++ graspy/__init__.py
@@ -3,8 +3,9 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
 warnings.simplefilter("always", category=UserWarning)
 
-import graspy.utils
-import graspy.embed
 import graspy.cluster
+import graspy.embed
+import graspy.inference
 import graspy.plot
 import graspy.simulations
+import graspy.utils
