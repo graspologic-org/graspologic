527a351e5c996691d93059ead2c645aa8e6c75e7
j1c <jaewonc78@gmail.com>
2019-02-05 18:38:10 -0500

    LSE sklearn compliant. Change is_symmetric to is_almost_symmetric for to_laplace

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -4,4 +4,5 @@ import numpy as np
 from sklearn.utils.estimator_checks import check_estimator
 
 check_estimator(graspy.embed.AdjacencySpectralEmbed)
+check_estimator(graspy.embed.LaplacianSpectralEmbed)
 check_estimator(graspy.embed.ClassicalMDS)
