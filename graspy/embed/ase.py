94acb9f5820ed8a895ff30c1f87a0bb2b3331631
Benjamin Pedigo <benjamindpedigo@gmail.com>
2019-02-19 23:40:01 -0500

    update description

--- graspy/embed/ase.py
+++ graspy/embed/ase.py
@@ -44,7 +44,8 @@ class AdjacencySpectralEmbed(BaseEmbed):
     check_lcc : bool , optional (defult = True)
         Whether to check if input graph is connected. May result in non-optimal 
         results if the graph is unconnected. If True and input is unconnected,
-        a UserWarning is thrown.
+        a UserWarning is thrown. Not checking for connectedness may result in 
+        faster computatoin.
 
     Attributes
     ----------
