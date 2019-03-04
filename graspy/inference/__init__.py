0f14f88ec54dc70f528d8e8638365fd0a2a74cf2
Vikram Chandrashekhar <vchandr6@jhu.edu>
2019-03-04 15:17:28 -0800

    exposed additional functions

--- graspy/inference/__init__.py
+++ graspy/inference/__init__.py
@@ -1,4 +1,4 @@
 from .semipar import SemiparametricTest
-from .sbm import estimate_dcsbm_parameters,estimate_sbm_parameters
+from .sbm import estimate_dcsbm_parameters,estimate_sbm_parameters,get_block_degrees,get_block_probabilities
 
 __all__ = ["SemiparametricTest"]
