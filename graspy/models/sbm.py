1262811f303228dd44330a108e5174ba1588ce2d
bdpedigo <benjamindpedigo@gmail.com>
2019-04-03 09:17:07 -0400

    models with bic

--- graspy/models/sbm.py
+++ graspy/models/sbm.py
@@ -1,8 +1,8 @@
 from .base import BaseGraphEstimator, _calculate_p, _fit_weights, cartprod
-from ..utils import import_graph
+from ..utils import import_graph, binarize
 import itertools
 import numpy as np
-from ..simulations import sbm
+from ..simulations import sbm, sample_edges
 
 
 class SBEstimator(BaseGraphEstimator):
@@ -16,28 +16,41 @@ class SBEstimator(BaseGraphEstimator):
         y : block assignments
         """
         if y is None:
+            # TODO:
             # _fit_block_assignments(graph)
             raise NotImplementedError("No a posteriori case yet")
             # would have to do some kind of GMM thingy here
+            # vertex labels = ...
+        else:
+            self.y = y
+            vertex_labels = y
 
         graph = import_graph(graph)
-        # self.n_vertices = graph.shape[0]
-
-        p = _calculate_p(graph)
-        self.p_ = p
+        self.n_verts = graph.shape[0]
 
         # at this point assume block assignments are known
-        self.vertex_labels_ = y
 
         # iterate over the blocks and calculate p
+        # TODO: need to do this part in such a way as to align the labels
+        # with a way that we can plot/sample
         block_labels, block_inv, block_sizes = np.unique(
-            self.vertex_labels_, return_inverse=True, return_counts=True
+            vertex_labels, return_inverse=True, return_counts=True
         )
+        # TODO: could sort everything by size here?
 
         self.block_sizes_ = block_sizes
 
         n_blocks = len(block_labels)
         block_inds = range(n_blocks)
+
+        block_members = []
+        for bs, bl in zip(block_sizes, block_labels):
+            block_members = block_members + bs * [bl]
+
+        # TODO:
+        # this is indexed in the same way as the internal model (unique)
+        # could use unique like here?
+        self.block_members_ = np.array(block_members)
 
         block_vert_inds = []
         for i in block_inds:
@@ -55,32 +68,118 @@ class SBEstimator(BaseGraphEstimator):
             block = graph[from_inds, :][:, to_inds]
             p = _calculate_p(block)
             block_p[from_block, to_block] = p
+
         self.block_p_ = block_p
 
+        # block_map = cartprod(block_members, block_members).T
+        block_map = cartprod(block_inv, block_inv).T
+        p_by_edge = block_p[block_map[0], block_map[1]]
+        p_mat = p_by_edge.reshape(graph.shape)
+        self.p_mat_ = p_mat
+
         if self.fit_degrees:
-            out_degree = graph.sum(axis=1)
-            in_degree = graph.sum(axis=0)
+            out_degree = np.count_nonzero(graph, axis=1)
+            in_degree = np.count_nonzero(graph, axis=0)
             degree_corrections = out_degree + in_degree
+            degree_corrections = degree_corrections.astype(float)
             inds = np.append(inds, [degree_corrections.size])
             for i in block_inds:
                 block_degrees = degree_corrections[block_vert_inds[i]]
-                degree_corrections[block_vert_inds[i]] /= block_degrees.sum()
+                degree_corrections[block_vert_inds[i]] = degree_corrections[
+                    block_vert_inds[i]
+                ] / np.mean(block_degrees)
+                # norm = np.mean(
+                #     np.outer(
+                #         degree_corrections[block_vert_inds[i]],
+                #         degree_corrections[block_vert_inds[i]],
+                #     )
+                # )
+                # print(norm)
+
             self.degree_corrections_ = degree_corrections
+            p_mat = p_mat * np.outer(degree_corrections, degree_corrections)
+            p_mat[p_mat > 1] = 1
+            self.p_mat_ = p_mat
         else:
             self.degree_corrections_ = None
 
         if self.fit_weights:
+            # TODO: something
             raise NotImplementedError("no weighted case yet")
             # _fit_weights(graph)
 
         return self
 
     def sample(self):
-        graph = sbm(
-            self.block_sizes_,
-            self.block_p_,
-            loops=self.loops,
-            directed=self.directed,
-            dc=self.degree_corrections_,
-        )
+        # graph = sbm(
+        #     self.block_sizes_,
+        #     self.block_p_,
+        #     loops=self.loops,
+        #     directed=self.directed,
+        #     dc=self.degree_corrections_,
+        # )
+        graph = sample_edges(self.p_mat_, directed=self.directed, loops=self.loops)
         return graph
+
+    def _n_parameters(self):
+        n_parameters = 0
+        n_parameters += self.block_p_.size  # elements in block p matrix
+        n_parameters += self.block_sizes_.size
+        if self.fit_degrees:
+            n_parameters += self.n_verts  # one degree correction per vert is stored
+            # TODO: more than this? becasue now the position of verts w/in block matters
+        return n_parameters
+
+    # def score(self, graph):
+    #     return super().score(graph)
+
+    # def score_samples(self, graph):
+    #     """
+    #     Compute the weighted log probabilities for each sample.
+    #     """
+    #     # for each edge
+    #     # look up where you are in the sbm
+    #     #   if dcsbm, look up where you are in the dcvector
+    #     #   take the product of those for your indices
+    #     # for each non edge, also neet to find 1 - p...
+
+    #     # if we had the p matrix ....
+    #     # this would be as simple as
+    #     # where graph is the unweighted graph:
+    #     # P.ravel() <dot> graph * (1 - P.ravel()) <dot> (1 - graph)
+    #     bin_graph = binarize(graph)
+    #     p_mat = self.p_mat_
+    #     successes = np.multiply(p_mat, bin_graph)
+    #     failures = np.multiply((1 - p_mat), (1 - bin_graph))
+    #     likelihood = successes + failures
+    #     return np.log(likelihood)
+
+    # def score_samples(self, graph):
+    #     """
+    #     Compute the weighted log probabilities for each sample.
+    #     """
+    #     # for each edge
+    #     # look up where you are in the sbm
+    #     #   if dcsbm, look up where you are in the dcvector
+    #     #   take the product of those for your indices
+    #     # for each non edge, also neet to find 1 - p...
+
+    #     # if we had the p matrix ....
+    #     # this would be as simple as
+    #     # where graph is the unweighted graph:
+    #     # P.ravel() <dot> graph * (1 - P.ravel()) <dot> (1 - graph)
+
+    # def score(self, graph):
+    #     """
+    #     Compute the per-sample average log-likelihood of the given data X.
+    #     """
+    #     return self.score_samples(graph).mean()
+
+    # def bic(self, graph):
+    #     """
+    #     do bic
+    #     """
+    #     return 1
+
+    # def calc_p_matrix(self):
+
--- notebooks/bpedigo/drosophila_demo.py
+++ notebooks/bpedigo/drosophila_demo.py
@@ -3,17 +3,126 @@
 from graspy.models import EREstimator, SBEstimator, RDPGEstimator
 from graspy.datasets import load_drosophila_left
 from graspy.plot import heatmap
+import numpy as np
 
 left_adj, cell_labels = load_drosophila_left(return_labels=True)
 left_adj_uw = left_adj.copy()
 left_adj_uw[left_adj_uw > 0] = 1
 
-heatmap(left_adj_uw, inner_hier_labels=cell_labels)
+# heatmap(left_adj_uw, inner_hier_labels=cell_labels)
 
-er = EREstimator(fit_degrees=False)
-er.fit(left_adj)
-print(er.degree_corrections_)
-heatmap(er.sample())
+models = [
+    EREstimator(fit_degrees=False),
+    EREstimator(fit_degrees=True),
+    SBEstimator(fit_degrees=False),
+    SBEstimator(fit_degrees=True),
+    RDPGEstimator(n_components=50),
+    RDPGEstimator(n_components=10),
+]
+
+for model in models:
+    m = model.fit(left_adj, y=cell_labels)
+    # heatmap(m.p_mat_, inner_hier_labels=cell_labels)
+    # heatmap(m.sample(), inner_hier_labels=cell_labels)
+    s = m.sample()
+    # print(m.score(s))
+    # print(m.score(left_adj))
+    # heatmap(m.score_samples(left_adj))
+    # print(m.bic(left_adj))
+    # print(m._n_parameters())
+    edge_log_likelihoods = m.score_samples(left_adj)
+    inds = np.where(np.logical_and(np.isneginf(edge_log_likelihoods), left_adj == 0))
+    print(len(inds[0]))
+    print(np.isneginf(edge_log_likelihoods).sum())
+    edge_log_likelihoods[np.isneginf(edge_log_likelihoods)] = 0
+    print(edge_log_likelihoods.sum())
+    p_mat = m.p_mat_
+    print(p_mat.max())
+    print(p_mat.min())
+    print(p_mat[p_mat == 1.0].sum())
+    print()
+    print()
+#%%
+# er = EREstimator(fit_degrees=False)
+# er.fit(left_adj)
+# heatmap(er.sample(), inner_hier_labels=cell_labels)
+
+# dcer = EREstimator(fit_degrees=True)
+# dcer.fit(left_adj)
+# heatmap(dcer.sample(), inner_hier_labels=cell_labels)
+
+# sbm = SBEstimator(fit_degrees=False)
+# sbm.fit(left_adj, y=cell_labels)
+# heatmap(sbm.sample(), inner_hier_labels=cell_labels)
+
+# dcsbm = SBEstimator(fit_degrees=True)
+# dcsbm.fit(left_adj, y=cell_labels)
+# d = dcsbm.sample()
+# heatmap(d, inner_hier_labels=cell_labels)
 
 
+# sbm_test = SBEstimator(fit_degrees=False)
+# sbm_test.fit(d, y=cell_labels)
+# sbm_test.block_p_
+# sbm.block_p_
+# heatmap(dcsbm.p_mat_)
+#%%
+from graspy.simulations import er_np
+
+n_sims = 10
+ll_m = np.zeros(n_sims)
+ll_e = []
+p = 0.2
+for i in range(n_sims):
+    g = er_np(100, p, directed=True, loops=True)
+    er = EREstimator(False)
+    er = er.fit(g)
+    log_likelihood_edges = er.score_samples(g)
+    log_likelihood_mean = er.score(g)
+    # er.bic(g)
+    ll_m[i] = log_likelihood_mean
+    ll_e.append(log_likelihood_edges)
+
+import seaborn as sns
+
+likelihood = np.exp(er.score_samples(g))
+likelihood.max()
+likelihood.min()
+heatmap(likelihood)
+heatmap(g)
+print(p * p + (1 - p) * (1 - p))
+print(ll_m.mean())
+# print(np.exp(ll_m.mean()))
+
+#%%
+def sbm_p_matrix(block_n, block_ps, dc=None):
+    block_members = []
+    for i, bs in enumerate(block_n):
+        block_members = block_members + bs * [i]
+    block_members = np.array(block_members)
+    block_map = cartprod(block_members, block_members).T
+    p_by_edge = block_ps[block_map[0], block_map[1]]
+    p_mat = p_by_edge.reshape((m.sum(), m.sum()))
+    return p_mat
+
+
+def cartprod(*arrays):
+    N = len(arrays)
+    return np.transpose(
+        np.meshgrid(*arrays, indexing="ij"), np.roll(np.arange(N + 1), -1)
+    ).reshape(-1, N)
+
+
+m = np.array([10, 20])
+bp = np.array([[0.5, 0.1], [0.05, 0.7]])
+sbm_p_mat = sbm_p_matrix(m, bp)
+dc_mat = np.outer(dc, dc)
+heatmap(dc_mat)
+heatmap(sbm_p_mat)
+#%%
+a = np.array([[0, 1], [0, 1]])
+b = np.arange(0, 20).reshape((2, 10))
+a
+b
+b[1, a]
 #%%
