090ae14f4e532603caac2b3e77414c57ed15af3a
bdpedigo <benjamindpedigo@gmail.com>
2019-04-03 16:57:23 -0400

    sims

--- notebooks/bpedigo/drosophila_demo.py
+++ notebooks/bpedigo/drosophila_demo.py
@@ -1,33 +1,108 @@
 #%%
-
 from graspy.models import EREstimator, SBEstimator, RDPGEstimator
 from graspy.datasets import load_drosophila_left
 from graspy.plot import heatmap
+from graspy.utils import symmetrize, binarize
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
+
+# ##################
+# plot_graphs = False
+# min_comp = 0
+# max_comp = 2
+# n_comp = 10
+# ##################
+
+## Load data
+sns.set_context("talk")
 left_adj, cell_labels = load_drosophila_left(return_labels=True)
 left_adj_uw = left_adj.copy()
 left_adj_uw[left_adj_uw > 0] = 1
 
-heatmap(left_adj_uw, inner_hier_labels=cell_labels)
+left_adj_uw = symmetrize(left_adj_uw, method="avg")
+left_adj_uw = binarize(left_adj_uw)
 
-models = [
-    EREstimator(fit_degrees=False),
-    EREstimator(fit_degrees=True),
-    SBEstimator(fit_degrees=False),
-    SBEstimator(fit_degrees=True),
-    RDPGEstimator(n_components=50),
-    RDPGEstimator(n_components=10),
-]
 
-for model in models:
-    m = model.fit(left_adj, y=cell_labels)
-    heatmap(m.p_mat_, inner_hier_labels=cell_labels)
-    heatmap(m.sample(), inner_hier_labels=cell_labels)
-    s = m.sample()
-    print(m.bic(left_adj))
+def evaluate_models(
+    graph, labels=None, plot_graphs=False, min_comp=0, max_comp=1, n_comp=5
+):
 
+    if plot_graphs:
+        heatmap(graph, inner_hier_labels=cell_labels)
+
+    ## Set up models to test
+    non_rdpg_models = [
+        EREstimator(fit_degrees=False),
+        EREstimator(fit_degrees=True),
+        SBEstimator(fit_degrees=False),
+        SBEstimator(fit_degrees=True),
+    ]
+
+    d = [int(i) for i in np.logspace(min_comp, max_comp, n_comp)]
+    rdpg_models = [RDPGEstimator(n_components=i) for i in d]
+    models = non_rdpg_models + rdpg_models
+
+    names_nonRDPG = ["ER", "DCER", "SBM", "DCSBM"]
+    names_RDPG = ["RDPG {}".format(i) for i in d]
+    names = names_nonRDPG + names_RDPG
+
+    bics = []
+    log_likelihoods = []
+
+    ## Test models
+    for model, name in zip(models, names):
+        m = model.fit(graph, y=labels)
+        if plot_graphs:
+            heatmap(m.p_mat_, inner_hier_labels=labels, title=(name + "P matrix"))
+            heatmap(m.sample(), inner_hier_labels=labels, title=(name + "sample"))
+        bic = m.bic(graph)
+        log_likelihoods.append(m.score(graph))
+        bics.append(bic)
+        plt.show()
+
+    bics = np.array(bics)
+    log_likelihoods = np.array(log_likelihoods)
+
+    ## Plot results
+    plt.figure()
+    fig, ax = plt.subplots(1, 2, sharey=True, figsize=(10, 10))
+    sns.pointplot(names_nonRDPG, bics[:4], join=False, ax=ax[0])
+    sns.scatterplot(d, bics[4:])
+    ax[1].set_xlabel("RDPG - d")
+    ax[0].set_xlabel("A priori models")
+    ax[0].set_ylabel("rBIC")
+    plt.suptitle("Drosophila left MB", y=0.94)
+
+    plt.figure()
+    fig, ax = plt.subplots(1, 2, sharey=True, figsize=(10, 10))
+    sns.pointplot(names_nonRDPG, -log_likelihoods[:4], join=False, ax=ax[0])
+    sns.scatterplot(d, -log_likelihoods[4:])
+    ax[1].set_xlabel("RDPG - d")
+    ax[0].set_xlabel("A priori models")
+    ax[0].set_ylabel("-ln(Likelihood)")
+    plt.suptitle("Drosophila left MB", y=0.94)
+
+    return bics, log_likelihoods
+
+
+evaluate_models(left_adj, labels=cell_labels)
 #%%
+heatmap(left_adj_uw)
+from graspy.embed import AdjacencySpectralEmbed
+
+ase = AdjacencySpectralEmbed(n_components=12)
+X = ase.fit_transform(left_adj_uw)
+X.max()
+P = X @ X.T
+P.max()
+P.min()
+#
+#
+# END
+
+##%%
 # debug version
 for model in models:
     m = model.fit(left_adj, y=cell_labels)
@@ -51,7 +126,7 @@ for model in models:
     # print(p_mat[p_mat == 1.0].sum())
     # print()
     # print()
-#%%
+##%%
 # er = EREstimator(fit_degrees=False)
 # er.fit(left_adj)
 # heatmap(er.sample(), inner_hier_labels=cell_labels)
