b4735371fd64923f8da2a09242c1a10e9754d9d2
Vikram Chandrashekhar <vikramc@jhmi.edu>
2019-04-08 13:56:25 -0400

    removed SBM and DC-SBM estimation code

--- graspy/inference/__init__.py
+++ graspy/inference/__init__.py
@@ -1,10 +1,4 @@
 from .semipar import SemiparametricTest
-from .sbm import (
-    estimate_dcsbm_parameters,
-    estimate_sbm_parameters,
-    get_block_degrees,
-    get_block_probabilities,
-)
 from .nonpar import NonparametricTest
 
 __all__ = ["SemiparametricTest", "NonparametricTest"]
