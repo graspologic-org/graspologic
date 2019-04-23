02e16d1bec239777463f679a7d5815e5f4a9aaa5
bdpedigo <benjamindpedigo@gmail.com>
2019-04-23 11:47:00 -0400

    formatting

--- graspy/utils/utils.py
+++ graspy/utils/utils.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # utils.py
 # Created by Eric Bridgeford on 2018-09-07.
 # Email: ebridge2@jhu.edu
@@ -308,7 +306,7 @@ def is_fully_connected(graph):
     ----------
     graph: nx.Graph, nx.DiGraph, nx.MultiDiGraph, nx.MultiGraph, np.ndarray
         Input graph in any of the above specified formats. If np.ndarray, 
-        interpreted as an :math: `n \times n` adjacency matrix
+        interpreted as an :math:`n \times n` adjacency matrix
 
     Returns
     -------
@@ -343,7 +341,7 @@ def get_lcc(graph, return_inds=False):
     ----------
     graph: nx.Graph, nx.DiGraph, nx.MultiDiGraph, nx.MultiGraph, np.ndarray
         Input graph in any of the above specified formats. If np.ndarray, 
-        interpreted as an :math: `n \times n` adjacency matrix
+        interpreted as an :math:`n \times n` adjacency matrix
     
     return_inds: boolean, default: False
         Whether to return a np.ndarray containing the indices in the original
@@ -448,7 +446,7 @@ def get_multigraph_intersect_lcc(graphs, return_inds=False):
     Parameters
     ----------
     graphs: list or np.ndarray
-        if list, each element must be an :math: `n \times n` np.ndarray adjacency matrix
+        if list, each element must be an :math:`n \times n` np.ndarray adjacency matrix
         
     return_inds: boolean, default: False
         Whether to return a np.ndarray containing the indices in the original
@@ -502,7 +500,7 @@ def get_multigraph_intersect_lcc(graphs, return_inds=False):
 def augment_diagonal(graph, weight=1):
     r"""
     Replaces the diagonal of adjacency matrix with 
-    :math: `\frac{degree}{num_verts - 1}` for the degree associated
+    :math:`\frac{degree}{num_verts - 1}` for the degree associated
     with each node. 
 
     For directed graphs, the degree used is the out degree (number) of 
