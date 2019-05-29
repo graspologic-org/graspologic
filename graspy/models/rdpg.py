b907848728178c2c9af2984d4988eb89552c7c96
bdpedigo <benjamindpedigo@gmail.com>
2019-05-29 10:04:10 -0400

    add rdpg docs [skip travis]

--- graspy/models/rdpg.py
+++ graspy/models/rdpg.py
@@ -5,6 +5,61 @@ from ..utils import import_graph, augment_diagonal, is_unweighted
 
 
 class RDPGEstimator(BaseGraphEstimator):
+    r"""
+    Random Dot Product Graph
+
+    Under the random dot product graph model, each node is assumed to have a
+    "latent position" in some :math:`d`-dimensional Euclidian space. This vector 
+    dictates that node's probability of connection to other nodes. For a given pair
+    of nodes :math:`i` and :math:`j`, the probability of connection is the dot 
+    product between their latent positions: 
+
+    :math:`P_{ij} = \langle x_i, x_j \rangle`
+
+    where :math:`x_i` and :math:`x_j` are the latent positions of nodes :math:`i` and
+    :math:`j`, respectively.
+
+    Parameters
+    ----------
+    loops : boolean, optional (default=False)
+        Whether to allow entries on the diagonal of the adjacency matrix, i.e. loops in 
+        the graph where a node connects to itself. 
+
+    n_components : int, optional (default=None)
+        The dimensionality of the latent space used to model the graph. If None, the
+        method of Zhu and Godsie will be used to select an embedding dimension. 
+    
+    ase_kws : dict, optional (default={})
+        Dictionary of keyword arguments passed down to 
+        `graspy.embed.AdjacencySpectralEmbed`, which is used to fit the model.
+
+    diag_aug_weight : int or float, optional (default=1)
+        Weighting used for diagonal augmentation, which is a form of regularization for 
+        fitting the RDPG model. 
+
+    plus_c_weight : int or float, optional (default=1)
+        Weighting used for a constant scalar added to the adjacency matrix before 
+        embedding as a form of regularization.
+
+    See also
+    --------
+    graspy.simulations.rdpg
+    graspy.embed.AdjacencySpectralEmbed
+    graspy.utils.augment_diagonal
+
+    References
+    ----------
+    .. [1] Athreya, A., Fishkind, D. E., Tang, M., Priebe, C. E., Park, Y.,
+           Vogelstein, J. T., ... & Sussman, D. L. (2018). Statistical inference
+           on random dot product graphs: a survey. Journal of Machine Learning 
+           Research, 18(226), 1-92.
+
+    .. [2] Zhu, M. and Ghodsi, A. (2006).
+           Automatic dimensionality selection from the scree plot via the use of
+           profile likelihood. Computational Statistics & Data Analysis, 51(2), 
+           pp.918-930.
+    """
+
     def __init__(
         self,
         loops=False,
