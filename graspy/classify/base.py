e8944c9bdeedb3706c5029fe6b35401e85d4dc57
Vikram Chandrashekhar <vikramc@jhmi.edu>
2019-06-03 09:35:52 -0400

    updated file headers to match. and removed comment

--- graspy/classify/base.py
+++ graspy/classify/base.py
@@ -1,6 +1,16 @@
-# base.py
-# Created by Vikram Chandrashekhar on 2019-03-05.
-# Email: vikramc@jhmi.edu
+# Copyright 2019 NeuroData (http://neurodata.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from abc import ABC, abstractmethod
 
@@ -34,7 +44,6 @@ class BaseSignalSubgraph(ABC, BaseEstimator, ClassifierMixin):
         --------
         
         """
-        # call self._reduce_dim(A) from your respective embedding technique.
         # import graph(s) to an adjacency matrix using import_graph function
         # here
         return self
