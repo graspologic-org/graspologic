8f5229e7a17a941c48bda70452fe4053519ff787
j1c <jaewonc78@gmail.com>
2019-06-08 14:09:17 +0200

    Squash commits

--- graspy/utils/utils.py
+++ graspy/utils/utils.py
@@ -44,15 +44,29 @@ def import_graph(graph):
     if isinstance(graph, (nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph)):
         out = nx.to_numpy_array(graph, nodelist=sorted(graph.nodes), dtype=np.float)
     elif isinstance(graph, (np.ndarray, np.memmap)):
-        maximum = np.max(graph.shape)
+        shape = graph.shape
+        if len(shape) > 3:
+            msg = "Input tensor must have at most 3 dimensions, not {}.".format(
+                len(shape)
+            )
+            raise ValueError(msg)
+        elif len(shape) == 3:
+            if shape[1] != shape[2]:
+                msg = "Input tensor must have same number of vertices."
+                raise ValueError(msg)
+            min_features = shape[1]
+            min_samples = 2
+        else:
+            min_features = np.max(shape)
+            min_samples = min_features
         out = check_array(
             graph,
             dtype=[np.float64, np.float32],
             ensure_2d=True,
             allow_nd=True,  # For omni tensor input
-            ensure_min_features=maximum,
-            ensure_min_samples=maximum,
-            copy=True,
+            ensure_min_features=min_features,
+            ensure_min_samples=min_samples,
+            copy=False,
         )
     else:
         msg = "Input must be networkx.Graph or np.array, not {}.".format(type(graph))
