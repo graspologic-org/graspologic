2f1866a945eb211b6360b8661c2a6c96ca04831c
j1c <jaewonc78@gmail.com>
2019-06-24 13:58:50 -0400

    Add randomness to tests, remove GMM error from pytest

--- tests/test_mase.py
+++ tests/test_mase.py
@@ -6,27 +6,31 @@ from graspy.embed.mase import MultipleASE
 from graspy.simulations.simulations import er_np, sbm
 
 
-def make_train_undirected(n=[20, 20], m=10):
+def make_train_undirected(n=[128, 128], m=10, alpha=1):
     """
     Make 4 class training dataset
     n = number of vertices
     m = number of graphs from each class
     """
-    p1 = [[1, 0], [0, 1]]
-    p2 = [[0, 1], [1, 0]]
-    p3 = [[1, 0], [0, 0]]
-    p4 = [[0, 0], [0, 1]]
+    c1 = np.array([[0.1, 0], [0, 0.1]])
+    c2 = -1 * c1
+    c3 = np.array([[0.1, 0], [0, 0]])
+    c4 = np.array([[0, 0], [0, 0.1]])
 
-    A = [sbm(n, p) for _ in range(m) for p in [p1, p2, p3, p4]]
+    A = [
+        sbm(n, np.ones((2, 2)) * 0.25 + alpha * c)
+        for _ in range(m)
+        for c in [c1, c2, c3, c4]
+    ]
 
     return A
 
 
-def make_train_directed(n=[20, 20], m=10):
-    p1 = [[0, 1], [0, 0]]
-    p2 = [[0, 0], [1, 0]]
-    p3 = [[1, 1], [0, 0]]
-    p4 = [[0, 0], [1, 1]]
+def make_train_directed(n=[128, 128], m=10):
+    p1 = [[0, 0.9], [0, 0]]
+    p2 = [[0, 0], [0.9, 0]]
+    p3 = [[0.9, 0.9], [0, 0]]
+    p4 = [[0, 0], [0.9, 0.9]]
 
     A = [sbm(n, p, directed=True) for _ in range(m) for p in [p1, p2, p3, p4]]
 
@@ -67,20 +71,20 @@ def test_graph_clustering():
     """
     # undirected case
     np.random.seed(2)
-    n = [20, 20]
+    n = [128, 128]
     m = 10
     X = make_train_undirected(n, m)
 
-    res = MultipleASE(n_components=2).fit(X).scores_.reshape((m * 4, -1))
-    gmm = GaussianCluster(10).fit(res)
+    res = MultipleASE(2).fit(X).scores_.reshape((m * 4, -1))
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
     assert gmm.n_components_ == 4
 
     # directed case
     np.random.seed(3)
     X = make_train_directed(n, m)
 
-    res = MultipleASE(n_components=2).fit(X).scores_.reshape((m * 4, -1))
-    gmm = GaussianCluster(10).fit(res)
+    res = MultipleASE(2).fit(X).scores_.reshape((m * 4, -1))
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
     assert gmm.n_components_ == 4
 
     # Scaled cases
@@ -88,32 +92,32 @@ def test_graph_clustering():
     np.random.seed(12)
     X = make_train_undirected(n, m)
 
-    res = MultipleASE(n_components=2, scaled=True).fit(X).scores_.reshape((m * 4, -1))
-    gmm = GaussianCluster(10).fit(res)
+    res = MultipleASE(2, scaled=True).fit(X).scores_.reshape((m * 4, -1))
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
     assert gmm.n_components_ == 4
 
     # directed case
     np.random.seed(13)
     X = make_train_directed(n, m)
 
-    res = MultipleASE(n_components=2, scaled=True).fit(X).scores_.reshape((m * 4, -1))
-    gmm = GaussianCluster(10).fit(res)
+    res = MultipleASE(2, scaled=True).fit(X).scores_.reshape((m * 4, -1))
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
     assert gmm.n_components_ == 4
 
 
 def test_vertex():
     """
-    There should be 2 clusters for undirected and 4 for directed.
+    There should be 2 clusters since each graph is a 2 block model
     """
     # undirected case
     np.random.seed(4)
-    n = [20, 20]
+    n = [128, 128]
     m = 10
     X = make_train_undirected(n, m)
 
     # undirected case
     res = MultipleASE(n_components=2).fit(X).latent_left_
-    gmm = GaussianCluster(1, 10).fit(res)
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
     assert gmm.n_components_ == 2
 
     # directed case
@@ -122,15 +126,15 @@ def test_vertex():
 
     mase = MultipleASE(n_components=2).fit(X)
     res = np.hstack([mase.latent_left_, mase.latent_right_])
-    gmm = GaussianCluster(1, 10).fit(res)
-    assert gmm.n_components_ == 4
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
+    assert gmm.n_components_ == 2
 
     # Scaled and undirected case
     np.random.seed(4)
     X = make_train_undirected(n, m)
 
     res = MultipleASE(n_components=2, scaled=True).fit_transform(X)
-    gmm = GaussianCluster(1, 10).fit(res)
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
     assert gmm.n_components_ == 2
 
     # Scaled and directed case
@@ -139,5 +143,5 @@ def test_vertex():
 
     left, right = MultipleASE(n_components=2, scaled=True).fit_transform(X)
     res = np.hstack([left, right])
-    gmm = GaussianCluster(1, 10).fit(res)
-    assert gmm.n_components_ == 2  # why is this 2?
+    gmm = GaussianCluster(10, covariance_type="all").fit(res)
+    assert gmm.n_components_ == 2
