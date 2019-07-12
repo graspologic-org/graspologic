b3223d839e8d0dd060a24c26ad9a4b7ae631f2ef
j1c <jaewonc78@gmail.com>
2019-07-12 16:09:48 -0400

    Exclude test folder

--- setup.py
+++ setup.py
@@ -55,5 +55,5 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
 )
