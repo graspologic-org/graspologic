0e20e80d9302f8487f5971f91ab98bbe1a6784ca
Sambit Panda <sampanda501@gmail.com>
2019-09-19 14:47:08 -0400

    add shell of classification code to graspy

--- graspy/__init__.py
+++ graspy/__init__.py
@@ -9,6 +9,7 @@ import graspy.pipeline
 import graspy.plot
 import graspy.simulations
 import graspy.utils
+import graspy.classification
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=PendingDeprecationWarning)
