45e8a2a4ecdf5b8659bd7cf0c40fa5f5ee02bc86
Shan Qiu <qiushan.thu@gmail.com>
2019-11-05 17:07:08 -0500

    black test_sims_corr.py

--- tests/test_sims_corr.py
+++ tests/test_sims_corr.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 import warnings
 
+
 class Test_Sample_Corr(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -17,20 +18,23 @@ class Test_Sample_Corr(unittest.TestCase):
         g1, g2 = sample_edges_corr(P, Rho, directed=False, loops=False)
         # check the 1 probability of the output binary matrix
         # should be near to the input P matrix
-        self.assertTrue(np.isclose(self.p, g2.sum() / (self.n * (self.n - 1)), atol = 0.05))
+        self.assertTrue(
+            np.isclose(self.p, g2.sum() / (self.n * (self.n - 1)), atol=0.05)
+        )
         # check rho in graph2
         add = g1 + g2
         add[add != 2] = 0
         real_prob = add.sum() / (2 * self.n * (self.n - 1))
-        real_rho = np.abs(real_prob - self.p**2) / (self.p - self.p**2)
-        self.assertTrue(np.isclose(self.rho, real_rho, atol = 0.06))
+        real_rho = np.abs(real_prob - self.p ** 2) / (self.p - self.p ** 2)
+        self.assertTrue(np.isclose(self.rho, real_rho, atol=0.06))
         # check the similarity of g1 and g2
-        judge = (g1 == g2)
+        judge = g1 == g2
         judge.astype(int)
         real_sim = (np.sum(judge) - self.n) / (self.n * (self.n - 1))
-        exp_sim = self.p*(self.p + self.rho * (1 - self.p)) + (1 - self.p) * (1 - self.p * (1 - self.rho))
-        self.assertTrue(np.isclose(exp_sim, real_sim, atol = 0.05))
+        exp_sim = self.p * (self.p + self.rho * (1 - self.p)) + (1 - self.p) * (
+            1 - self.p * (1 - self.rho)
+        )
+        self.assertTrue(np.isclose(exp_sim, real_sim, atol=0.05))
         # check the dimension of input P and Rho
         self.assertTrue(g1.shape == (self.n, self.n))
         self.assertTrue(g2.shape == (self.n, self.n))
-
