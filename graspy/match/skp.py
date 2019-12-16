c719b7ab15e330b3603e65f5686b9d1a2df464c9
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2019-12-16 14:08:46 -0500

    Update skp.py

--- graspy/match/skp.py
+++ graspy/match/skp.py
@@ -1,3 +1,32 @@
+# Title: sinkhorn_knopp Source Code
+# Author: Tabanpour, B
+# Date: 2018
+# Code version:  0.2
+# Availability: https://pypi.org/project/sinkhorn_knopp/
+#
+# The MIT License
+#
+# Copyright (c) 2016 Baruch Tabanpour
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
 import warnings
 import numpy as np
 
@@ -90,6 +119,7 @@ class SinkhornKnopp:
             is convertible to a numpy array. The matrix must not be
             equal to 0 and it must have total support for the algorithm
             to converge.
+
         Returns
         -------
         P_eps : A double stochastic matrix.
