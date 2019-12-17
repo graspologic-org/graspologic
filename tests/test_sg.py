558dacbbbde726bc6cee2159f71ca3a0dd6db736
gk <gkang@jh.edu>
2019-12-16 21:39:03 -0500

    Update test_sg.py

--- tests/test_sg.py
+++ tests/test_sg.py
@@ -1,4 +1,4 @@
-import graspy.subgraphing as sg
+import graspy.subgraph as sg
 import unittest
 import numpy as np
 from numpy.testing import assert_equal
@@ -14,7 +14,8 @@ class TestEstimateSubgraph(unittest.TestCase):
         for ind in range(10):
             if ys[ind] == 1:
                 A[:, :, ind] = blank
-        estsub = sg.estimate_signal_subgraph(A, ys, [5, 1])
+        test_model = sg.SignalSubgraph(A,ys)
+        estsub = test_model.fit_transform([5, 1])
         ver = np.ones((10, 10))
         ver[estsub] = 0
         np.testing.assert_array_equal(blank, ver)
@@ -28,46 +29,50 @@ class TestEstimateSubgraph(unittest.TestCase):
         for ind in range(10):
             if ys[ind] == 1:
                 A[:, :, ind] = blank
-        estsub = sg.estimate_signal_subgraph(A, ys, 5)
+        test_model = sg.SignalSubgraph(A,ys)
+        estsub = test_model.fit_transform(5)
         ver = np.ones((10, 10))
         ver[estsub] = 0
         np.testing.assert_array_equal(blank, ver)
 
-    def test_estimate_subgraph_bad_constraints(self):
+    def test_fit_bad_constraints(self):
         A = np.ones((5, 5, 5))
         ys = np.ones(5)
+        test_model = sg.SignalSubgraph(A,ys)
         with self.assertRaises(TypeError):
-            sg.estimate_signal_subgraph(A, ys, [1])
+            test_model.fit([1])
         with self.assertRaises(TypeError):
-            sg.estimate_signal_subgraph(A, ys, [1, 1, 1])
+            test_model.fit([1, 1, 1])
 
-
-class TestConstructContingency(unittest.TestCase):
     def test_construct_contingency(self):
         A = np.ones((1, 1, 5))
         A[:, :, 1::2] = 0
         ys = np.array([1, 0, 1, 0, 0])
-        cmat = sg.construct_contingency(A, ys)
+        test_model = sg.SignalSubgraph(A,ys)
+        test_model._SignalSubgraph__construct_contingency()
+        cmat = test_model.contmat
         ver = np.array([[[[1, 2], [2, 0]]]], dtype=float)
         np.testing.assert_array_equal(cmat, ver)
 
-    def test_construct_contingency_bad_type(self):
+    def test_fit_bad_type(self):
         A = [[[1 for i in range(5)] for j in range(5)] for k in range(5)]
         ys = [1, 1, 1, 1, 1]
         with self.assertRaises(TypeError):
-            sg.construct_contingency(A, np.ones(5))
+            sg.SignalSubgraph(A, np.ones(5))
+        with self.assertRaises(TypeError):
+            sg.SignalSubgraph(A, set(ys))
 
-    def test_construct_contingency_bad_size(self):
+    def test_fit_bad_size(self):
         with self.assertRaises(ValueError):
-            sg.construct_contingency(np.ones((5, 5)), np.ones(5))
+            sg.SignalSubgraph(np.ones((5, 5)), np.ones(5))
         with self.assertRaises(ValueError):
-            sg.construct_contingency(np.ones((3, 4, 2)), np.ones(2))
+            sg.SignalSubgraph(np.ones((3, 4, 2)), np.ones(2))
 
-    def test_construct_contingency_bad_len(self):
+    def test_fit_bad_len(self):
         A = np.ones((3, 3, 3))
         with self.assertRaises(ValueError):
-            sg.construct_contingency(A, np.ones((3, 3)))
+            sg.SignalSubgraph(A, np.ones((3, 3)))
         with self.assertRaises(ValueError):
-            sg.construct_contingency(A, np.array([0, 1, 2]))
+            sg.SignalSubgraph(A, np.array([0, 1, 2]))
         with self.assertRaises(ValueError):
-            sg.construct_contingency(A, np.ones(2))
+            sg.SignalSubgraph(A, np.ones(2))
