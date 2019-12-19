1e5797e0c7b1a077d48366669b300972959273ca
j1c <jaewonc78@gmail.com>
2019-12-19 12:03:08 -0500

    Black

--- graspy/plot/__init__.py
+++ graspy/plot/__init__.py
@@ -2,4 +2,5 @@ import sys
 import matplotlib as mpl
 
 from .plot import heatmap, gridplot, pairplot, degreeplot, edgeplot, screeplot
+
 __all__ = ["heatmap", "gridplot", "pairplot", "degreeplot", "edgeplot", "screeplot"]
