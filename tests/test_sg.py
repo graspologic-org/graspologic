52715dc840eca8036a1e7ba365bf004c4d53e723
gk <gkang@jh.edu>
2019-12-19 21:47:20 -0500

    Update test_sg.py

--- tests/test_sg.py
+++ tests/test_sg.py
@@ -14,8 +14,8 @@ class TestEstimateSubgraph(unittest.TestCase):
         for ind in range(10):
             if ys[ind] == 1:
                 A[:, :, ind] = blank
-        test_model = sg.SignalSubgraph(A, ys)
-        estsub = test_model.fit_transform([5, 1])
+        test_model = sg.SignalSubgraph()
+        estsub = test_model.fit_transform(A,ys,[5, 1])
         ver = np.ones((10, 10))
         ver[estsub] = 0
         np.testing.assert_array_equal(blank, ver)
@@ -29,8 +29,8 @@ class TestEstimateSubgraph(unittest.TestCase):
         for ind in range(10):
             if ys[ind] == 1:
                 A[:, :, ind] = blank
-        test_model = sg.SignalSubgraph(A, ys)
-        estsub = test_model.fit_transform(5)
+        test_model = sg.SignalSubgraph()
+        estsub = test_model.fit_transform(A,ys,5)
         ver = np.ones((10, 10))
         ver[estsub] = 0
         np.testing.assert_array_equal(blank, ver)
@@ -38,41 +38,45 @@ class TestEstimateSubgraph(unittest.TestCase):
     def test_fit_bad_constraints(self):
         A = np.ones((5, 5, 5))
         ys = np.ones(5)
-        test_model = sg.SignalSubgraph(A, ys)
+        test_model = sg.SignalSubgraph()
         with self.assertRaises(TypeError):
-            test_model.fit([1])
+            test_model.fit(A,ys,[1])
         with self.assertRaises(TypeError):
-            test_model.fit([1, 1, 1])
+            test_model.fit(A,ys,[1, 1, 1])
 
     def test_construct_contingency(self):
         A = np.ones((1, 1, 5))
         A[:, :, 1::2] = 0
         ys = np.array([1, 0, 1, 0, 0])
-        test_model = sg.SignalSubgraph(A, ys)
+        test_model = sg.SignalSubgraph()
+        test_model.fit(A,ys,1)
         test_model._SignalSubgraph__construct_contingency()
-        cmat = test_model.contmat
+        cmat = test_model.contmat_
         ver = np.array([[[[1, 2], [2, 0]]]], dtype=float)
         np.testing.assert_array_equal(cmat, ver)
 
     def test_fit_bad_type(self):
         A = [[[1 for i in range(5)] for j in range(5)] for k in range(5)]
         ys = [1, 1, 1, 1, 1]
+        test_model = sg.SignalSubgraph()
         with self.assertRaises(TypeError):
-            sg.SignalSubgraph(A, np.ones(5))
+            test_model.fit(A, np.ones(5),1)
         with self.assertRaises(TypeError):
-            sg.SignalSubgraph(A, set(ys))
+            test_model.fit(A, set(ys),1)
 
     def test_fit_bad_size(self):
+        test_model = sg.SignalSubgraph()
         with self.assertRaises(ValueError):
-            sg.SignalSubgraph(np.ones((5, 5)), np.ones(5))
+            test_model.fit(np.ones((5, 5)), np.ones(5),1)
         with self.assertRaises(ValueError):
-            sg.SignalSubgraph(np.ones((3, 4, 2)), np.ones(2))
+            test_model.fit(np.ones((3, 4, 2)), np.ones(2),1)
 
     def test_fit_bad_len(self):
         A = np.ones((3, 3, 3))
+        test_model = sg.SignalSubgraph()
         with self.assertRaises(ValueError):
-            sg.SignalSubgraph(A, np.ones((3, 3)))
+            test_model.fit(A, np.ones((3, 3)),1)
         with self.assertRaises(ValueError):
-            sg.SignalSubgraph(A, np.array([0, 1, 2]))
+            test_model.fit(A, np.array([0, 1, 2]),1)
         with self.assertRaises(ValueError):
-            sg.SignalSubgraph(A, np.ones(2))
+            test_model.fit(A, np.ones(2),1)
