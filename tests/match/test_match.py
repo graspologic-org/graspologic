8e78dc9320621830ad0ad11180ad49678f554162
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2020-02-07 12:56:03 -0500

    Update test_match.py

--- tests/match/test_match.py
+++ tests/match/test_match.py
@@ -1,7 +1,9 @@
 import pytest
 import numpy as np
 import math
+import random
 from graspy.match import FastApproximateQAP as FAQ
+from graspy.match import SeededGraphMatching as SGM
 from graspy.match import SinkhornKnopp as SK
 
 
@@ -96,6 +98,107 @@ class TestFAQ:
         chr15a = self.rand.fit(A, B)
         score = chr15a.score_
         assert 9896 <= score < 11500
+
+class TestSGM:
+    @classmethod
+    def setup_class(cls):
+        cls.barycenter = SGM()
+        cls.rand = SGM(n_init=100, init_method="rand")
+
+    def test_SGM_inputs(self):
+        with pytest.raises(TypeError):
+            SGM(n_init=-1.5)
+        with pytest.raises(ValueError):
+            SGM(init_method="random")
+        with pytest.raises(TypeError):
+            SGM(max_iter=-1.5)
+        with pytest.raises(TypeError):
+            SGM(shuffle_input="hey")
+        with pytest.raises(TypeError):
+            SGM(eps=-1)
+        with pytest.raises(TypeError):
+            SGM(gmp="hey")
+        with pytest.raises(ValueError):
+            SGM().fit(np.random.random((3, 4)), np.random.random((3, 4)),np.arange(2),np.arange(2))
+        with pytest.raises(ValueError):
+            SGM().fit(np.random.random((3, 3)), np.random.random((4, 4)),np.arange(2),np.arange(2))
+        with pytest.raises(ValueError):
+            SGM().fit(np.random.random((3, 4)), np.random.random((3, 4)),np.arange(2),np.arange(2))
+        with pytest.raises(ValueError):
+            SGM().fit(-1 * np.identity(3), -1 * np.identity(3),np.arange(2),np.arange(2))
+        with pytest.raises(ValueError):
+            SGM().fit(np.identity(3), np.identity(3), np.identity(3), np.arange(2))
+        with pytest.raises(ValueError):
+            SGM().fit(np.identity(3), np.identity(3), np.arange(1), np.arange(2))
+        with pytest.raises(ValueError):
+            SGM().fit(np.identity(3), np.identity(3), np.arange(5), np.arange(5))
+        with pytest.raises(ValueError):
+            SGM().fit(np.identity(3), np.identity(3), -1 * np.arange(2), -1 * np.arange(2))
+
+
+    def _get_AB(self, qap_prob):
+        with open("tests/match/qapdata/" + qap_prob + ".dat") as f:
+            f = [int(elem) for elem in f.read().split()]
+
+                    # adjusting
+            f = np.array(f[1:])
+            n = int(math.sqrt(len(f) / 2))
+            f = f.reshape(2 * n, n)
+            A = f[:n, :]
+            B = f[n:, :]
+            return A, B
+
+    def test_barycenter_SGM(self):
+        A, B = self._get_AB("chr12c")
+        n = A.shape[0]
+        pi = np.array([7, 5, 1, 3, 10, 4, 8, 6, 9, 11, 2, 12]) - [1] * n
+        W1 = np.sort(random.sample(list(range(n)), 3))
+        W2 = [pi[z] for z in W1]
+        chr12c = self.barycenter.fit(A, B,W1,W2)
+        score = chr12c.score_
+        assert 11156 <= score < 20000
+
+        W1 = np.sort(random.sample(list(range(n)), n-1))
+        W2 = [pi[z] for z in W1]
+        chr12c = self.barycenter.fit(A, B, W1, W2)
+        score = chr12c.score_
+        assert 11156 == score
+
+        A, B = self._get_AB("chr15a")
+        n = A.shape[0]
+        pi = np.array([5,10,8,13,12,11,14,2,4,6,7,15,3,1,9]) - [1] * n
+        W1 = np.sort(random.sample(list(range(n)), 3))
+        W2 = [pi[z] for z in W1]
+        chr15a = self.barycenter.fit(A, B, W1, W2)
+        score = chr15a.score_
+        assert 9896 <= score < 20000
+
+        W1 = np.sort(random.sample(list(range(n)), n - 1))
+        W2 = [pi[z] for z in W1]
+        chr15a = self.barycenter.fit(A, B, W1, W2)
+        score = chr15a.score_
+        assert 9896 == score
+
+    def test_rand_SGM(self):
+        A, B = self._get_AB("chr12c")
+        n = A.shape[0]
+        pi = np.array([7, 5, 1, 3, 10, 4, 8, 6, 9, 11, 2, 12]) - [1] * n
+        W1 = np.sort(random.sample(list(range(n)), 3))
+        W2 = [pi[z] for z in W1]
+        chr12c = self.rand.fit(A, B, W1, W2)
+        score = chr12c.score_
+        assert 11156 <= score < 12000
+
+        A, B = self._get_AB("chr15a")
+        n = A.shape[0]
+        pi = np.array([5, 10, 8, 13, 12, 11, 14, 2, 4, 6, 7, 15, 3, 1, 9]) - [1] * n
+        W1 = np.sort(random.sample(list(range(n)), 4))
+        W2 = [pi[z] for z in W1]
+        chr15a = self.rand.fit(A, B, W1, W2)
+        score = chr15a.score_
+        assert 9896 <= score < 10500
+
+
 
 
 class TestSinkhornKnopp:
