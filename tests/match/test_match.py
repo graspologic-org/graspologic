e55e82767c923e3fe744ee37544d793ff0509437
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2020-03-04 12:43:53 -0500

    Update test_match.py

--- tests/match/test_match.py
+++ tests/match/test_match.py
@@ -2,52 +2,78 @@ import pytest
 import numpy as np
 import math
 import random
-from graspy.match import FastApproximateQAP as FAQ
-from graspy.match import SeededGraphMatching as SGM
+from graspy.match import GraphMatch as GMP
 from graspy.match import SinkhornKnopp as SK
 
 
-class TestFAQ:
+class TestGMP:
     @classmethod
     def setup_class(cls):
-        cls.barycenter = FAQ()
-        cls.rand = FAQ(n_init=100, init_method="rand")
+        cls.barycenter = GMP(gmp=False)
+        cls.rand = GMP(n_init=100, init_method="rand", gmp=False)
 
-    def test_FAQ_inputs(self):
+    def test_SGM_inputs(self):
         with pytest.raises(TypeError):
-            FAQ(n_init=-1.5)
+            GMP(n_init=-1.5)
         with pytest.raises(ValueError):
-            FAQ(init_method="random")
+            GMP(init_method="random")
         with pytest.raises(TypeError):
-            FAQ(max_iter=-1.5)
+            GMP(max_iter=-1.5)
         with pytest.raises(TypeError):
-            FAQ(shuffle_input="hey")
+            GMP(shuffle_input="hey")
         with pytest.raises(TypeError):
-            FAQ(eps=-1)
+            GMP(eps=-1)
         with pytest.raises(TypeError):
-            FAQ(gmp="hey")
+            GMP(gmp="hey")
         with pytest.raises(ValueError):
-            FAQ().fit(np.random.random((3, 4)), np.random.random((3, 4)))
+            GMP().fit(
+                np.random.random((3, 4)),
+                np.random.random((3, 4)),
+                np.arange(2),
+                np.arange(2),
+            )
         with pytest.raises(ValueError):
-            FAQ().fit(np.random.random((3, 3)), np.random.random((4, 4)))
+            GMP().fit(
+                np.random.random((3, 3)),
+                np.random.random((4, 4)),
+                np.arange(2),
+                np.arange(2),
+            )
         with pytest.raises(ValueError):
-            FAQ().fit(np.random.random((3, 4)), np.random.random((3, 4)))
+            GMP().fit(
+                np.random.random((3, 4)),
+                np.random.random((3, 4)),
+                np.arange(2),
+                np.arange(2),
+            )
         with pytest.raises(ValueError):
-            FAQ().fit(-1 * np.identity(3), -1 * np.identity(3))
+            GMP().fit(
+                -1 * np.identity(3), -1 * np.identity(3), np.arange(2), np.arange(2)
+            )
+        with pytest.raises(ValueError):
+            GMP().fit(np.identity(3), np.identity(3), np.identity(3), np.arange(2))
+        with pytest.raises(ValueError):
+            GMP().fit(np.identity(3), np.identity(3), np.arange(1), np.arange(2))
+        with pytest.raises(ValueError):
+            GMP().fit(np.identity(3), np.identity(3), np.arange(5), np.arange(5))
+        with pytest.raises(ValueError):
+            GMP().fit(
+                np.identity(3), np.identity(3), -1 * np.arange(2), -1 * np.arange(2)
+            )
 
     def _get_AB(self, qap_prob):
         with open("tests/match/qapdata/" + qap_prob + ".dat") as f:
             f = [int(elem) for elem in f.read().split()]
 
             # adjusting
-        f = np.array(f[1:])
-        n = int(math.sqrt(len(f) / 2))
-        f = f.reshape(2 * n, n)
-        A = f[:n, :]
-        B = f[n:, :]
-        return A, B
+            f = np.array(f[1:])
+            n = int(math.sqrt(len(f) / 2))
+            f = f.reshape(2 * n, n)
+            A = f[:n, :]
+            B = f[n:, :]
+            return A, B
 
-    def test_barycenter(self):
+    def test_barycenter_SGM(self):
         A, B = self._get_AB("lipa20a")
         lipa20a = self.barycenter.fit(A, B)
         score = lipa20a.score_
@@ -68,106 +94,6 @@ class TestFAQ:
         score = lipa30b.score_
         assert score == 151426
 
-        A, B = self._get_AB("lipa40a")
-        lipa40a = self.barycenter.fit(A, B)
-        score = lipa40a.score_
-        assert 31538 <= score < 32300
-
-        A, B = self._get_AB("lipa40b")
-        lipa40b = self.barycenter.fit(A, B)
-        score = lipa40b.score_
-        assert score == 476581
-
-        A, B = self._get_AB("lipa50a")
-        lipa50a = self.barycenter.fit(A, B)
-        score = lipa50a.score_
-        assert 62093 <= score < 63300
-
-        A, B = self._get_AB("lipa50b")
-        lipa50b = self.barycenter.fit(A, B)
-        score = lipa50b.score_
-        assert score == 1210244
-
-    def test_rand(self):
-        A, B = self._get_AB("chr12c")
-        chr12c = self.rand.fit(A, B)
-        score = chr12c.score_
-        assert 11156 <= score < 13500
-
-        A, B = self._get_AB("chr15a")
-        chr15a = self.rand.fit(A, B)
-        score = chr15a.score_
-        assert 9896 <= score < 12000
-
-
-class TestSGM:
-    @classmethod
-    def setup_class(cls):
-        cls.barycenter = SGM(gmp=False)
-        cls.rand = SGM(n_init=100, init_method="rand", gmp=False)
-
-    def test_SGM_inputs(self):
-        with pytest.raises(TypeError):
-            SGM(n_init=-1.5)
-        with pytest.raises(ValueError):
-            SGM(init_method="random")
-        with pytest.raises(TypeError):
-            SGM(max_iter=-1.5)
-        with pytest.raises(TypeError):
-            SGM(shuffle_input="hey")
-        with pytest.raises(TypeError):
-            SGM(eps=-1)
-        with pytest.raises(TypeError):
-            SGM(gmp="hey")
-        with pytest.raises(ValueError):
-            SGM().fit(
-                np.random.random((3, 4)),
-                np.random.random((3, 4)),
-                np.arange(2),
-                np.arange(2),
-            )
-        with pytest.raises(ValueError):
-            SGM().fit(
-                np.random.random((3, 3)),
-                np.random.random((4, 4)),
-                np.arange(2),
-                np.arange(2),
-            )
-        with pytest.raises(ValueError):
-            SGM().fit(
-                np.random.random((3, 4)),
-                np.random.random((3, 4)),
-                np.arange(2),
-                np.arange(2),
-            )
-        with pytest.raises(ValueError):
-            SGM().fit(
-                -1 * np.identity(3), -1 * np.identity(3), np.arange(2), np.arange(2)
-            )
-        with pytest.raises(ValueError):
-            SGM().fit(np.identity(3), np.identity(3), np.identity(3), np.arange(2))
-        with pytest.raises(ValueError):
-            SGM().fit(np.identity(3), np.identity(3), np.arange(1), np.arange(2))
-        with pytest.raises(ValueError):
-            SGM().fit(np.identity(3), np.identity(3), np.arange(5), np.arange(5))
-        with pytest.raises(ValueError):
-            SGM().fit(
-                np.identity(3), np.identity(3), -1 * np.arange(2), -1 * np.arange(2)
-            )
-
-    def _get_AB(self, qap_prob):
-        with open("tests/match/qapdata/" + qap_prob + ".dat") as f:
-            f = [int(elem) for elem in f.read().split()]
-
-            # adjusting
-            f = np.array(f[1:])
-            n = int(math.sqrt(len(f) / 2))
-            f = f.reshape(2 * n, n)
-            A = f[:n, :]
-            B = f[n:, :]
-            return A, B
-
-    def test_barycenter_SGM(self):
         A, B = self._get_AB("chr12c")
         n = A.shape[0]
         pi = np.array([7, 5, 1, 3, 10, 4, 8, 6, 9, 11, 2, 12]) - [1] * n
@@ -199,6 +125,16 @@ class TestSGM:
         assert 9896 == score
 
     def test_rand_SGM(self):
+        A, B = self._get_AB("chr12c")
+        chr12c = self.rand.fit(A, B)
+        score = chr12c.score_
+        assert 11156 <= score < 13500
+
+        A, B = self._get_AB("chr15a")
+        chr15a = self.rand.fit(A, B)
+        score = chr15a.score_
+        assert 9896 <= score < 12000
+
         A, B = self._get_AB("chr12c")
         n = A.shape[0]
         pi = np.array([7, 5, 1, 3, 10, 4, 8, 6, 9, 11, 2, 12]) - [1] * n
