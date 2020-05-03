7ed7f709d36f3ee025a3ac9c2cd5d483fd83f756
SHAAAAN <qiushan.thu@gmail.com>
2020-05-03 17:31:38 -0400

    another try

--- graspy/simulations/__init__.py
+++ graspy/simulations/__init__.py
@@ -1,7 +1,37 @@
-from .simulations import sample_edges, er_np, er_nm, sbm, rdpg
-from .simulations_corr import sample_edges_corr, er_corr, sbm_corr
+from .simulations import (
+    _n_to_labels,
+    sample_edges,
+    er_np,
+    er_nm,
+    sbm,
+    rdpg,
+    p_from_latent,
+)
+from .simulations_corr import (
+    sample_edges_corr,
+    er_corr,
+    sbm_corr,
+    check_dirloop,
+    check_r,
+    check_rel_er,
+    check_rel_sbm,
+)
 from .rdpg_corr import rdpg_corr
 
-__all__ = ["sample_edges", "er_np", "er_nm", "sbm", "rdpg",
-    "sample_edges_corr", "er_corr", "sbm_corr",
-    "rdpg_corr"]
+__all__ = [
+    "_n_to_labels",
+    "sample_edges",
+    "er_np",
+    "er_nm",
+    "sbm",
+    "rdpg",
+    "p_from_latent",
+    "check_dirloop",
+    "check_r",
+    "check_rel_er",
+    "check_rel_sbm",
+    "sample_edges_corr",
+    "er_corr",
+    "sbm_corr",
+    "rdpg_corr",
+]
