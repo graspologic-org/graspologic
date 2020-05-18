0b8aa50ed4f5bcb5bd2eb26f0dca1018bcd26d8f
Anton Alyakin <alyakin314@gmail.com>
2020-05-18 19:06:33 +0300

    fixed hyppo versions in requirements and setup

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ REQUIRED_PACKAGES = [
     "scipy>=1.1.0",
     "seaborn>=0.9.0",
     "matplotlib>=3.0.0",
-    "hyppo @ git+https://github.com/neurodata/hyppo@master#egg=hyppo",
+    "hyppo>=0.1.2",
 ]
 
 
