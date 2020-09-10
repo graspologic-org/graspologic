21aee86c65932e26a0f1155a51a729381bbac703
Kareef Ullah <kareefullah@macbook-pro.myfiosgateway.com>
2020-09-09 23:11:57 -0400

    only change for rdpg_corr

--- graspy/simulations/__init__.py
+++ graspy/simulations/__init__.py
@@ -1,3 +1,3 @@
-from .simulations import sample_edges, er_np, er_nm, sbm, rdpg, p_from_latent
-from .simulations_corr import sample_edges_corr, er_corr, sbm_corr
+from .simulations import *
+from .simulations_corr import *
 from .rdpg_corr import rdpg_corr
