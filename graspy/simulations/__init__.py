aaafe8e6bfc79d0a0130ebe2e1b5fb1e05aa0fdd
Kareef Ullah <kareefullah@macbook-pro.myfiosgateway.com>
2020-09-12 23:40:44 -0400

    Remove all *

--- graspy/simulations/__init__.py
+++ graspy/simulations/__init__.py
@@ -1,3 +1,16 @@
-from .simulations import *
-from .simulations_corr import *
+from .simulations import sample_edges, er_np, er_nm, sbm, rdpg, p_from_latent
+from .simulations_corr import sample_edges_corr, er_corr, sbm_corr
 from .rdpg_corr import rdpg_corr
+
+__all__ = [
+    "sample_edges",
+    "er_np",
+    "er_nm",
+    "sbm",
+    "rdpg",
+    "p_from_latent",
+    "sample_edges_corr",
+    "er_corr",
+    "sbm_corr",
+    "rdpg_corr",
+]
