4161bd7462f76bdc465c7b84e4753c7a032d8c74
Kareef Ullah <kareefullah@macbook-pro.myfiosgateway.com>
2020-09-17 13:09:05 -0400

    Revert to original

--- graspy/simulations/__init__.py
+++ graspy/simulations/__init__.py
@@ -2,7 +2,6 @@ from .simulations import sample_edges, er_np, er_nm, sbm, rdpg, p_from_latent
 from .simulations_corr import sample_edges_corr, er_corr, sbm_corr
 from .rdpg_corr import rdpg_corr
 
-# temp
 __all__ = [
     "sample_edges",
     "er_np",
