04a56d49d3df323745d50a23dc342bf1226bcef9
Kareef Ullah <kareefullah@macbook-pro.myfiosgateway.com>
2020-09-18 19:11:31 -0400

    typo

--- graspy/simulations/__init__.py
+++ graspy/simulations/__init__.py
@@ -6,7 +6,7 @@ from .simulations_corr import sample_edges_corr, er_corr, sbm_corr
 from .rdpg_corr import rdpg_corr
 
 __all__ = [
-    "sample edges",
+    "sample_edges",
     "er_np",
     "er_nm",
     "sbm",
