92420b276ab9ecb50550a32b8b0e7d16824496af
dfrancisco1998 <40680427+dfrancisco1998@users.noreply.github.com>
2020-09-24 18:14:17 -0400

    added in the MS lic and fix #461

--- graspy/subgraph/__init__.py
+++ graspy/subgraph/__init__.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
+
 from .sg import SignalSubgraph
 
 __all__ = [SignalSubgraph]
--- graspy/utils/__init__.py
+++ graspy/utils/__init__.py
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
 from .utils import (
     import_graph,
     import_edgelist,
