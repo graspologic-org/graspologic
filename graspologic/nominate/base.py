fdf5384845131b737e558de79f136429e077da5e
Spencer R Loggia <spencer.loggia98@gmail.com>
2020-10-04 14:18:41 -0400

    add VN base class

--- graspologic/nominate/base.py
+++ graspologic/nominate/base.py
@@ -1,1 +1,23 @@
-# Placehold
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
+
+from abc import abstractmethod
+from sklearn.base import BaseEstimator
+
+
+class BaseVN(BaseEstimator):
+
+    def __init__(self, multigraph=False):
+        self.multigraph = multigraph
+
+    @abstractmethod
+    def fit(self, X, y):
+        return None
+
+    @abstractmethod
+    def predict(self):
+        return None
+
+    @abstractmethod
+    def fit_transform(self, X, y=None):
+        return None
