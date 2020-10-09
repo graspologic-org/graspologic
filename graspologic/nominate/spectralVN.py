3cc53fe4ae20b9ff8b2125d4ae1d963973740bd8
Spencer R Loggia <spencer.loggia98@gmail.com>
2020-10-08 21:30:50 -0400

    prune for initial PR

--- graspologic/nominate/spectralVN.py
+++ graspologic/nominate/spectralVN.py
@@ -11,13 +11,29 @@ from scipy.spatial import distance
 from scipy.stats import mode
 
 
-class BaseSpectralVN(BaseVN):
+# STATIC METHODS #
+
+def _make_2d(cls, arr):
+    arr = np.array(arr, dtype=np.int)
+    if np.ndim(arr) < 2:
+        arr = np.concatenate((arr, np.zeros(arr.shape[0])))
+        arr = arr.reshape(-1, 2)
+    else:
+        arr = arr.reshape(-1, 2)
+    return arr
+
+
+class SpectralVertexNominator(BaseVN):
+    """
+
+    """
+
     def __init__(
-        self,
-        multigraph: bool,
-        embedding: np.ndarray,
-        embeder: Union[str, BaseEmbed],
-        mode: str,
+            self,
+            multigraph: bool = False,
+            embedding: np.ndarray = None,
+            embeder: Union[str, BaseEmbed] = "ASE",
+            mode: str = "single_vertex",
     ):
         super().__init__(multigraph=multigraph)
         self.embedding = embedding
@@ -32,6 +48,7 @@ class BaseSpectralVN(BaseVN):
         self._attr_labels = None
         self.unique_att = None
         self.mode = mode
+        self.distance_matrix = None
 
     def _pairwise_dist(self, y: np.ndarray, metric="euclidean") -> np.ndarray:
         # wrapper for scipy's cdist function
@@ -54,7 +71,7 @@ class BaseSpectralVN(BaseVN):
         if self.embedding is None:
             self.embedding = self.embeder.fit_transform(X)
 
-    def _fit(self, X: np.ndarray, y: np.ndarray):
+    def fit(self, X, y):
         """
         Constructs the embedding if needed.
         Parameters
@@ -73,48 +90,6 @@ class BaseSpectralVN(BaseVN):
         self._attr_labels = y[:, 1]
         self.seed = y[:, 0]
         self.unique_att = np.unique(self._attr_labels)
-
-    @classmethod
-    def _make_2d(cls, arr):
-        arr = np.array(arr, dtype=np.int)
-        if np.ndim(arr) < 2:
-            arr = np.concatenate((arr, np.zeros(arr.shape[0])))
-            arr = arr.reshape(-1, 2)
-        else:
-            arr = arr.reshape(-1, 2)
-        return arr
-
-    @abstractmethod
-    def fit(self, X, y):
-        pass
-
-    @abstractmethod
-    def predict(self) -> np.ndarray:
-        pass
-
-    @abstractmethod
-    def fit_transform(self, X, y=None):
-        pass
-
-
-class SpectralVertexNominator(BaseSpectralVN):
-    def __init__(
-        self,
-        multigraph: bool = False,
-        embedding: np.ndarray = None,
-        embeder: Union[str, BaseEmbed] = "ASE",
-        mode: str = "single_vertex",
-    ):
-        super(SpectralVertexNominator, self).__init__(
-            multigraph=multigraph, embedding=embedding, embeder=embeder, mode=mode
-        )
-        self.distance_matrix = None
-
-    def fit(self, X, y):
-        # detect if y is attributed, reshape if not.
-        y = BaseSpectralVN._make_2d(y)
-        self._fit(X, y)
-        self.distance_matrix = self._pairwise_dist(y)
 
     def _knn_simple_predict(self):
         """
@@ -194,75 +169,3 @@ class SpectralVertexNominator(BaseSpectralVN):
     def fit_transform(self, X, y=None):
         self.fit(X, y)
         return self.predict()
-
-
-class SpectralClusterVertexNominator(BaseSpectralVN):
-    def __init__(
-        self,
-        multigraph: bool = False,
-        embedding: np.ndarray = None,
-        embeder: Union[str, BaseEmbed] = "ASE",
-        mode: str = "single_vertex",
-    ):
-        super(SpectralClusterVertexNominator, self).__init__(
-            multigraph=multigraph, embedding=embedding, embeder=embeder, mode=mode
-        )
-        self.clf = None
-
-    def fit(self, X, y):
-        """
-        Unsupervised kmeans spectral nomination, as described
-        by Fishkind et. al. in Vertex Nomination for Membership Prediction.
-        Has the advantage of being able to identify attributes not represented
-        in seed population, however by default will select number of clusters
-        based on number of unique attributes in given seed.
-        Returns
-        -------
-
-        """
-        from sklearn.cluster import KMeans
-
-        # detect if y is attributed, reshape if not.
-        y = BaseSpectralVN._make_2d(y)
-        self._fit(X, y)
-        self.clf = KMeans(n_clusters=self.unique_att.shape[0])
-        self.clf.fit(self.embedding)
-
-    def _cluster_map(self, y_hat):
-        clusters = np.sort(np.unique(y_hat))
-        metric_arr = np.zeros((clusters.shape[0], clusters.shape[0]), dtype=np.float32)
-        for i in range(clusters.shape[0]):
-            att_ind = np.argwhere(y_hat == clusters[i]).reshape(-1)
-            _, seed_arg, _ = np.intersect1d(self.seed, att_ind, return_indices=True)
-            temp_att = np.concatenate(
-                (self._attr_labels[seed_arg], self.unique_att), axis=0
-            )  # ensure all rep'ed in count
-            id, counts = np.unique(temp_att, return_counts=True)
-            ind = np.argsort(id)
-            counts = counts[ind]  # leave some smoothing bias here to prevent nan slice
-            metric_arr[i] = counts / temp_att.shape[0]  # percent each label att
-        map = np.zeros(clusters.shape[0], dtype=np.int)
-        # assign cluster mapping greedily in percent match
-        for i in range(clusters.shape[0]):
-            best = np.unravel_index(np.nanargmax(metric_arr), metric_arr.shape)
-            og_att = np.sort(self.unique_att)[best[1]]
-            map[best[0].astype(int)] = og_att.astype(int)
-            metric_arr[best[0]] = np.nan
-            metric_arr[:, best[1]] = np.nan
-        return map
-
-    def predict(self, out="per_attribute"):
-        y_hat = self.clf.predict(self.embedding)
-        clust_to_att = self._cluster_map(y_hat)
-        centered_map = clust_to_att - np.min(
-            clust_to_att
-        )  # allows to order prediction as they were provided
-        clust_dists = self.clf.transform(self.embedding)
-        att_preds = np.empty(clust_dists.shape, dtype=np.int)
-        for i in range(clust_dists.shape[1]):
-            sort_inds = np.argsort(clust_dists[:, i])
-            att_preds[:, i] = sort_inds
-            clust_dists[:, i] = clust_dists[sort_inds, i]
-        att_preds = att_preds.T[centered_map].T
-        clust_dists = clust_dists.T[centered_map].T
-        return att_preds, clust_dists, np.sort(clust_to_att)
