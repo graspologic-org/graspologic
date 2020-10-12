011bc5a91cb9e3bd9e34aed2f2ad23184dd0daa8
loftusa <alexloftus2004@gmail.com>
2020-10-12 08:29:37 -0700

    never saved merge conflict fix

--- tests/test_sklearn.py
+++ tests/test_sklearn.py
@@ -6,11 +6,6 @@ import numpy as np
 
 from sklearn.utils.estimator_checks import check_estimator
 
-<<<<<<< HEAD
-check_estimator(graspologic.embed.AdjacencySpectralEmbed)  # TODO: figure out a better solution here
-=======
-# TODO: figure out a better solution here
-# check_estimator(graspologic.embed.AdjacencySpectralEmbed)  # with current implementation of predict, this class is no longer sklearn compliant
->>>>>>> 58c2d46107e22c2156f1649e7f7127251c6c91c2
+check_estimator(graspologic.embed.AdjacencySpectralEmbed)  
 check_estimator(graspologic.embed.LaplacianSpectralEmbed)
 check_estimator(graspologic.embed.ClassicalMDS)
