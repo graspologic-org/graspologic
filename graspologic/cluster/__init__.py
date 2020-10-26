881d6330c935c11537a969e47983ffd330e7f256
Benjamin Pedigo <benjamindpedigo@gmail.com>
2020-10-26 11:00:20 -0400

    Update __init__.py

--- graspologic/cluster/__init__.py
+++ graspologic/cluster/__init__.py
@@ -4,6 +4,6 @@
 from .gclust import GaussianCluster
 from .kclust import KMeansCluster
 from .autogmm import AutoGMMCluster
-from .rclust import DivisiveCluster
+from .divisive_cluster import DivisiveCluster
 
 __all__ = ["GaussianCluster", "KMeansCluster", "AutoGMMCluster", "DivisiveCluster"]
