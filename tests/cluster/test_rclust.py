27afd5709a1bfa136c568907433b401cc0b1ecfd
tliu68 <54865879+tliu68@users.noreply.github.com>
2020-10-26 15:02:54 +0800

    Update test_rclust.py

--- tests/cluster/test_rclust.py
+++ tests/cluster/test_rclust.py
@@ -7,7 +7,7 @@ from numpy.testing import assert_allclose, assert_array_less, assert_equal
 from sklearn.exceptions import NotFittedError
 from sklearn.metrics import adjusted_rand_score
 
-from graspologic.cluster import RecursiveCluster
+from graspologic.cluster import DivisiveCluster
 
 
 def test_inputs():
@@ -16,56 +16,56 @@ def test_inputs():
 
     # min_components < 1
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(min_components=0)
+        dc = DivisiveCluster(min_components=0)
 
     # min_components not integer
     with pytest.raises(TypeError):
-        rc = RecursiveCluster(min_components="1")
+        dc = DivisiveCluster(min_components="1")
 
     # max_components < min_components
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(min_components=1, max_components=0)
+        dc = DivisiveCluster(min_components=1, max_components=0)
 
     # max_components not integer
     with pytest.raises(TypeError):
-        rc = RecursiveCluster(max_components="1")
+        dc = DivisiveCluster(max_components="1")
 
     # cluster_method not in ['gmm', 'kmeans']
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(cluster_method="graspologic")
+        dc = DivisiveCluster(cluster_method="graspologic")
 
     # delta_criter negative
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(delta_criter=-1)
+        dc = DivisiveCluster(delta_criter=-1)
 
     # cluster_kws not a dict
     with pytest.raises(TypeError):
-        rc = RecursiveCluster(cluster_kws=0)
+        dc = DivisiveCluster(cluster_kws=0)
 
     # level not an int
     with pytest.raises(TypeError):
-        rc = RecursiveCluster(max_components=2)
-        rc.fit_predict(X, level="1")
-
-    with pytest.raises(TypeError):
-        rc = RecursiveCluster(max_components=2)
-        rc.fit(X)
-        rc.predict(X, level="1")
+        dc = DivisiveCluster(max_components=2)
+        dc.fit_predict(X, level="1")
 
     # level not positive
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(max_components=2)
-        rc.fit_predict(X, level=0)
+        dc = DivisiveCluster(max_components=2)
+        dc.fit_predict(X, level=0)
 
+    # level exceeds n_level
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(max_components=2)
-        rc.fit(X)
-        rc.predict(X, level=0)
+        dc = DivisiveCluster(max_components=2)
+        dc.fit_predict(X, fcluster=True, level=100)
+
+    # level is given but fcluster disabled
+    with pytest.raises(ValueError):
+        dc = DivisiveCluster(max_components=2)
+        dc.fit_predict(X, level=1)
 
     # max_components > n_sample
     with pytest.raises(ValueError):
-        rc = RecursiveCluster(max_components=101)
-        rc.fit(X)
+        dc = DivisiveCluster(max_components=101)
+        dc.fit(X)
 
 
 def test_predict_without_fit():
@@ -73,8 +73,8 @@ def test_predict_without_fit():
     X = np.random.normal(0, 1, size=(100, 3))
 
     with pytest.raises(NotFittedError):
-        rc = RecursiveCluster(max_components=2)
-        rc.predict(X)
+        dc = DivisiveCluster(max_components=2)
+        dc.predict(X)
 
 
 def test_predict_on_nonfitted_data_gmm():
@@ -87,8 +87,8 @@ def test_predict_on_nonfitted_data_gmm():
     X = np.vstack((X1, X2))
     y = np.repeat([0, 1], n)
 
-    rc = RecursiveCluster(max_components=2)
-    pred1 = rc.fit_predict(X)
+    dc = DivisiveCluster(max_components=2)
+    pred1 = dc.fit_predict(X)
 
     # Generate random data to predict on
     np.random.seed(2)
@@ -99,7 +99,7 @@ def test_predict_on_nonfitted_data_gmm():
     X_new = np.vstack((X1_new, X2_new))
     y_new = np.repeat([0, 1], n)
 
-    pred2 = rc.predict(X_new)
+    pred2 = dc.predict(X_new)
 
     # Assert that both predictions have the same depth
     assert_equal(pred1.shape[1], pred2.shape[1])
@@ -125,8 +125,8 @@ def test_predict_on_nonfitted_data_kmeans():
     X = np.vstack((X1, X2))
     y = np.repeat([0, 1], n)
 
-    rc = RecursiveCluster(max_components=2, cluster_method="kmeans")
-    pred1 = rc.fit_predict(X)
+    dc = DivisiveCluster(max_components=2, cluster_method="kmeans")
+    pred1 = dc.fit_predict(X)
 
     # Generate random data to predict on
     np.random.seed(2)
@@ -137,7 +137,7 @@ def test_predict_on_nonfitted_data_kmeans():
     X_new = np.vstack((X1_new, X2_new))
     y_new = np.repeat([0, 1], n)
 
-    pred2 = rc.predict(X_new)
+    pred2 = dc.predict(X_new)
 
     # Assert that both predictions have the same depth
     assert_equal(pred1.shape[1], pred2.shape[1])
@@ -182,8 +182,8 @@ def _test_hierarchical_four_class(**kws):
     Clustering above hierarchical data with gmm
     """
     np.random.seed(1)
-    rc = RecursiveCluster(max_components=2, **kws)
-    pred = rc.fit_predict(X)
+    dc = DivisiveCluster(max_components=2, **kws)
+    pred = dc.fit_predict(X, fcluster=True)
 
     # Assert that the 2-cluster model is the best at level 1
     assert_equal(np.max(pred[:, 0]) + 1, 2)
@@ -234,12 +234,12 @@ def test_hierarchical_six_class_delta_criter():
     y_lvl2 = np.repeat([0, 1, 2, 3, 4, 5], n)
 
     # Perform clustering without setting delta_criter
-    rc = RecursiveCluster(max_components=2)
-    pred = rc.fit_predict(X)
+    dc = DivisiveCluster(max_components=2)
+    pred = dc.fit_predict(X, fcluster=True)
 
     # Perform clustering while setting delta_criter
-    rc = RecursiveCluster(max_components=2, delta_criter=10)
-    pred_delta_criter = rc.fit_predict(X)
+    dc = DivisiveCluster(max_components=2, delta_criter=10)
+    pred_delta_criter = dc.fit_predict(X, fcluster=True)
 
     # Assert that pred has more levels than pred_delta_criter
     assert_equal(pred.shape[1] - 1, pred_delta_criter.shape[1])
