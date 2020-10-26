5f9e1e601e4adff8ab736de8813d050a86f9df6f
tliu68 <54865879+tliu68@users.noreply.github.com>
2020-10-26 15:08:35 +0800

    Update __init__.py

--- graspologic/cluster/__init__.py
+++ graspologic/cluster/__init__.py
@@ -4,6 +4,6 @@
 from .gclust import GaussianCluster
 from .kclust import KMeansCluster
 from .autogmm import AutoGMMCluster
-from .rclust import RecursiveCluster
+from .rclust import DivisiveCluster
 
 __all__ = ["GaussianCluster", "KMeansCluster", "AutoGMMCluster", "DivisiveCluster"]
