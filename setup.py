edb473ee394a33c94aaac96cf771ed61472048fb
dfrancisco1998 <40680427+dfrancisco1998@users.noreply.github.com>
2020-12-07 17:05:38 -0600

    make seborn .11 in setup.py

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         "networkx>=2.1",
         "numpy>=1.8.1",
         "POT>=0.7.0",
-        "seaborn>=0.9.0",
+        "seaborn>= 0.11.0",
         "scikit-learn>=0.19.1",
         "scipy>=1.4.0",
     ],
