4433b7147d21f74c51f64f17a63850dec5eeab79
Daniel Borders <danielborders@MacBook-Pro-2.local>
2020-12-17 20:28:38 -0600

    added blank notes section

--- graspologic/nominate/VNviaSGM.py
+++ graspologic/nominate/VNviaSGM.py
@@ -66,7 +66,8 @@ class VNviaSGM(BaseEstimator):
     .. [1] Patsolic, HG, Park, Y, Lyzinski, V, Priebe, CE. Vertex nomination via seeded graph matching. Stat Anal Data
         Min: The ASA Data Sci Journal. 2020; 13: 229– 244. https://doi.org/10.1002/sam.11454
 
-
+    Notes
+    -----
 
     """
 
