366bf675ed6b95fc8d7d3b2af88c9b40be73f71d
loftusa <alexloftus2004@gmail.com>
2021-02-27 15:29:21 -0500

    proper bounds on tuning parameter

--- graspologic/embed/case.py
+++ graspologic/embed/case.py
@@ -1,76 +1,81 @@
 # %%
 from graspologic.utils import import_graph, to_laplacian
 from graspologic.embed.base import BaseSpectralEmbed
-from graspologic.embed.lse import LaplacianSpectralEmbed
-from graspologic.simulations import sbm
-from graspologic.plot import heatmap
-
+from graspologic.embed.svd import selectSVD
 import numpy as np
-from scipy.linalg import eigh
+import scipy
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-import seaborn as sns
+from graspologic.plot import heatmap
+from graspologic.utils import remap_labels
+
+np.set_printoptions(suppress=True)
+
+# %%
+
+# %%
+
+#%%
 
 
 class CovariateAssistedEmbedding(BaseSpectralEmbed):
     """
-    Perform Spectral Embedding on a graph with covariates, using the regularized graph Laplacian.
+        Perform Spectral Embedding on a graph with covariates, using the regularized graph Laplacian.
 
-    The Covariate-Assisted Spectral Embedding is a k-dimensional Euclidean representation
-    of a graph based on a function of its Laplacian and a vector of covariate features
-    for each node.
+        The Covariate-Assisted Spectral Embedding is a k-dimensional Euclidean representation
+        of a graph based on a function of its Laplacian and a vector of covariate features
+        for each node.
 
-    Parameters
-    ----------
-    embedding_alg : str, default = "assortative"
-        Embedding algorithm to use:
-        - "assortative": Embed ``L + a*X@X.T``. Better for assortative graphs.
-        - "non-assortative": Embed ``L@L + a*X@X.T``. Better for non-assortative graphs.
-        - "cca": Embed ``L@X``. Better for large graphs.
+        Parameters
+        ----------
+        embedding_alg : str, default = "assortative"
+            Embedding algorithm to use:
+            - "assortative": Embed ``L + a*X@X.T``. Better for assortative graphs.
+            - "non-assortative": Embed ``L@L + a*X@X.T``. Better for non-assortative graphs.
+            - "cca": Embed ``L@X``. Better for large graphs and faster.
+    `
+        n_components : int or None, default = None
+            Desired dimensionality of output data. If "full",
+            n_components must be <= min(X.shape). Otherwise, n_components must be
+            < min(X.shape). If None, then optimal dimensions will be chosen by
+            ``select_dimension`` using ``n_elbows`` argument.
 
-    n_components : int or None, default = None
-        Desired dimensionality of output data. If "full",
-        n_components must be <= min(X.shape). Otherwise, n_components must be
-        < min(X.shape). If None, then optimal dimensions will be chosen by
-        ``select_dimension`` using ``n_elbows`` argument.
+        alpha : float, optional (default = None)
+            Tuning parameter to use. Not used if embedding_alg == cca:
+                -  None: tune the alpha-value by minimizing the k-means objective function.
+                         Since this involves running k-means over a parameter space, this
+                         will result in a slower algorithm in exchange for likely better
+                         clustering.
+                - float: use a particular alpha-value. Results in a much faster algorithm
+                         (since we are not tuning with kmeans) in exchange for potentially
+                         suboptimal clustering results.
+                -    -1: Default to the ratio of the leading eigenvector of the Laplacian
+                         to the leading eigenvector of the covariate matrix. This will
+                         result in suboptimal clustering in exchange for increased
+                         clustering speed.
 
-    alpha : float, optional (default = None)
-        Tuning parameter to use. Not used if embedding_alg == cca:
-            -  None: tune the alpha-value by minimizing the k-means objective function. 
-                     Since this involves running k-means over a parameter space, this 
-                     will result in a slower algorithm in exchange for likely better 
-                     clustering.
-            - float: use a particular alpha-value. Results in a much faster algorithm 
-                     (since we are not tuning with kmeans) in exchange for potentially
-                     suboptimal clustering results.
-            -    -1: Default to the ratio of the leading eigenvector of the Laplacian
-                     to the leading eigenvector of the covariate matrix. This will
-                     result in suboptimal clustering in exchange for increased
-                     clustering speed.
+        n_iter : int, optional (default = 100)
+            If tuning alpha with k-means, this parameter determines the number of times
+            k-means is run. Higher values are more computationally expensive in exchange
+            for a finer-grained search of the parameter space.
 
-    n_iter : int, optional (default = 100)
-        If tuning alpha with k-means, this parameter determines the number of times
-        k-means is run. Higher values are more computationally expensive in exchange
-        for a finer-grained search of the parameter space.
+        n_elbows : int, optional, default: 2
+            If `n_components=None`, then compute the optimal embedding dimension using
+            `select_dimension`. Otherwise, ignored.
 
-    n_elbows : int, optional, default: 2
-        If `n_components=None`, then compute the optimal embedding dimension using
-        `select_dimension`. Otherwise, ignored.
+        check_lcc : bool , optional (defult =True)
+            Whether to check if input graph is connected. May result in non-optimal
+            results if the graph is unconnected. Not checking for connectedness may
+            result in faster computation.
 
-    check_lcc : bool , optional (defult =True)
-        Whether to check if input graph is connected. May result in non-optimal
-        results if the graph is unconnected. Not checking for connectedness may
-        result in faster computation.
-
-    concat : bool, optional (default = False)
-        If graph(s) are directed, whether to concatenate each graph's left and right
-        (out and in) latent positions along axis 1.
+        concat : bool, optional (default = False)
+            If graph(s) are directed, whether to concatenate each graph's left and right
+            (out and in) latent positions along axis 1.
 
 
-    References
-    ---------
-    .. [1] Binkiewicz, N., Vogelstein, J. T., & Rohe, K. (2017). Covariate-assisted
-    spectral clustering. Biometrika, 104(2), 361-377.
+        References
+        ---------
+        .. [1] Binkiewicz, N., Vogelstein, J. T., & Rohe, K. (2017). Covariate-assisted
+        spectral clustering. Biometrika, 104(2), 361-377.
     """
 
     def __init__(
@@ -81,14 +86,14 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
         n_components=None,
         n_elbows=2,
         check_lcc=False,
-        concat=False
+        concat=False,
     ):
         super().__init__(
             algorithm="full",
             n_components=n_components,
             n_elbows=n_elbows,
             check_lcc=check_lcc,
-            concat=concat
+            concat=concat,
         )
 
         if embedding_alg not in {"assortative", "non-assortative", "cca"}:
@@ -113,7 +118,7 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
         .. math:: L_ = L + \alpha XX^T
         .. math:: L_ = LX
 
-        where :math:`\alpha` is a tuning parameter which makes the leading eigenvectors
+        where :math:`\alpha` is a tuning parameter which makes the leading eigenvalues
         of the two summands the same. Here, :math:`L` is the regularized
         graph Laplacian, and :math:`X` is a matrix of covariates for each node.
 
@@ -145,25 +150,25 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
 
         # change params based on tuning algorithm
         if self.embedding_alg == "cca":
-            self._LL = self._L@self._X
+            self._LL = self._L @ self._X
             self._XXt = self._X
             self.alpha_ = 0
-        if self.embedding_alg == "assortative":
+        elif self.embedding_alg == "assortative":
             self._LL = self._L
-            self._XXt = self._X@self._X.T
+            self._XXt = self._X @ self._X.T
             self.alpha_ = self._get_tuning_parameter()
         elif self.embedding_alg == "non-assortative":
-            self._LL = self._L@self._L
-            self._XXt = self._X@self._X.T
+            self._LL = self._L @ self._L
+            self._XXt = self._X @ self._X.T
             self.alpha_ = self._get_tuning_parameter()
 
-        self._embed()
+        # self._embed(plot=True)
         self.is_fitted_ = True
         return self
 
     def transform(self, X):
         return self._fit_transform(fit=False)
-        
+
     def _get_tuning_parameter(self):
         """
         Find an alpha which causes the leading eigenvectors of L@L and a*X@X.T to be
@@ -184,24 +189,33 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
         # setup
         if isinstance(self.alpha, (int, float)) and self.alpha != -1:
             return self.alpha
-        K = self.n_components  # number of clusters
-        R = self._R  # number of covariates
-        I = int(R <= K)
+        n_clusters = self.n_components  # number of clusters
+        n_cov = self._R  # number of covariates
+        I = int(n_cov <= n_clusters)
         LL = self._LL
         XXt = self._XXt
 
-        # add a 0 to eigvals for 1-indexing (easier to deal with), sort descending
-        L_eigvals = np.concatenate((0, np.flip(np.linalg.eigvalsh(LL))), axis=None)
-        X_eigvals = np.concatenate((0, np.flip(np.linalg.eigvalsh(XXt))), axis=None)
+        # grab eigenvalues
+        _, D, _ = selectSVD(self._X, n_components=self._X.shape[1], algorithm="full")
+        X_eigvals = D[0 : np.min([n_cov, n_clusters])]
+        _, D, _ = selectSVD(self._L, n_components=n_clusters + 1)
+        L_eigvals = D[0 : n_clusters + 1]
+        if self.embedding_alg == "non-assortative":
+            L_eigvals = L_eigvals ** 2
 
         # calculate bounds
-        L_top = L_eigvals[1]
-        X_top = X_eigvals[1]
-        amin = (L_eigvals[K] - L_eigvals[K+1])/X_top
-        amax = L_top / ((X_eigvals[R] * I) + ((X_eigvals[K] - X_eigvals[K+1])*(1-I)))
+        L_top = L_eigvals[0]
+        X_top = X_eigvals[0]
+        amin = (L_eigvals[n_clusters - 1] - L_eigvals[n_clusters]) / X_top ** 2
+        if n_cov > n_clusters:
+            amax = L_top / (X_eigvals[n_blocks - 1] ** 2 - X_eigvals[n_blocks] ** 2)
+        else:
+            amax = L_top / X_eigvals[n_cov - 1] ** 2
+
+        print(f"{amin=:.9f}, {amax=:.9f}")
 
         if self.alpha == -1:
-            # just use the ratio of the leading eigenvalues for the 
+            # just use the ratio of the leading eigenvalues for the
             # tuning parameter, or the closest value in its possible range.
             alpha = np.float(L_top / X_top)
             if amin <= alpha <= amax:
@@ -214,45 +228,106 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
         # run kmeans clustering and set alpha to the value
         # which minimizes clustering intertia
         # TODO: optimize... maybe with sklearn.metrics.make_scorer
-        #       and a GridSearch? 
+        #       and a GridSearch?
         alpha_range = np.linspace(amin, amax, num=self.n_iter)
         inertias = {}
         for a in alpha_range:
             kmeans = KMeans(n_clusters=self.n_components, n_jobs=-1)
             X_ = self._embed(alpha=a)
             kmeans.fit(X_)
-            print(f"inertia at {a}: {kmeans.inertia_}")
+            print(f"inertia at {a:.5f}: {kmeans.inertia_:.5f}")
             inertias[a] = kmeans.inertia_
         alpha = min(inertias, key=inertias.get)
+        print(f"Best inertia at {alpha:5f}: {inertias[alpha]:5f}")
         return alpha
 
-
-    def _embed(self, alpha=None):
+    def _embed(self, alpha=None, plot=False):
         if alpha is None:
             alpha = self.alpha_
 
         L_ = self._LL + alpha * (self._XXt)
+        if plot:
+            import matplotlib.pyplot as plt
+
+            # sns.heatmap(self._X, ax=axs[0, 0])
+            # heatmap(self._XXt)
+            # heatmap(self._L)
+            # heatmap(self._LL)
+            heatmap(L_)
         self._reduce_dim(L_)
         return self.latent_left_
 
 
-# def gen_covariates(labels, m1, m2, agreement=1, d=3):
-#     """
-#     n x 3 matrix of covariates
+def gen_covariates(labels, m1=0.8, m2=0.2, agreement=1, d=3):
+    """
+    n x 3 matrix of covariates
 
-#     """
-#     N = len(labels)
+    """
+    N = len(labels)
+    d = 3
+    B = np.full((d, d), m2)
+    B[np.diag_indices_from(B)] = m1
+    base = np.eye(d)
+    membership = np.zeros((N, d))
+    n_misassign = 0
+    for i in range(0, N):
+        assign = bool(np.random.binomial(1, agreement))
+        if assign:
+            membership[i, :] = base[labels[i], :]
+        else:
+            membership[i, :] = base[(labels[i] + 1) % (max(labels) + 1), :]
+            n_misassign += 1
 
-#     # Generate dynamic (e.g., random) covariate array
-#     dynamic = np.random.choice([1, 0], p=[m2, 1-m2], size=(N, d))
-#     m1_array = np.random.choice([1, 0], p=[m1, 1-m1], size=N)
-#     dynamic[np.arange(N), labels] = m1_array
-#     return dynamic
+    probs = membership @ B
 
-# from graspologic.simulations import sbm
-# from graspologic.plot import heatmap, pairplot
-# A, labels = sbm([100, 100, 100], p=[[.1, .9, .9], [.9, .1, .9], [.9, .9, .1]], return_labels=True)
-# X = gen_covariates(labels, m1=.8, m2=.2) 
-# case = CovariateAssistedEmbedding(n_components=3, embedding_alg="non-assortative")
-# Xhat = case.fit_transform(A, covariates=X)
-# pairplot(Xhat, labels=labels)
+    covariates = np.zeros(probs.shape)
+    for i in range(N):
+        for j in range(d):
+            covariates[i, j] = np.random.binomial(1, probs[i, j])
+
+    return covariates
+
+
+def get_misclustering(A, model, labels, covariates=None) -> float:
+    if covariates is None:
+        Xhat = model.fit_transform(A)
+    else:
+        Xhat = model.fit_transform(A, covariates=covariates)
+
+    kmeans = KMeans(n_clusters=3)
+    labels_ = kmeans.fit_predict(Xhat)
+
+    # to account for nonidentifiability
+    labels_ = remap_labels(labels, labels_)
+    misclustering = np.count_nonzero(labels - labels_) / len(labels)
+
+    return misclustering
+
+
+from graspologic.simulations import sbm
+from graspologic.utils import remap_labels
+from graspologic.plot import pairplot
+
+n = 100
+assortative = False
+p, q = 0.03, 0.015
+if not assortative:
+    p, q = q, p
+A, labels = sbm(
+    [n, n, n],
+    p=[[p, q, q], [q, p, q], [q, q, p]],
+    return_labels=True,
+)
+X = gen_covariates(
+    labels,
+    m1=0.8,
+    m2=0.2,
+)
+import seaborn as sns
+
+case = CovariateAssistedEmbedding(
+    n_components=3, embedding_alg="non-assortative", n_iter=5
+)
+Xhat = case.fit_transform(A, covariates=X)
+#%%
+pairplot(Xhat, labels=labels)
