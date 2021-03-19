d19f60c910779ca755cd532288e8b9e431d09cf2
loftusa <alexloftus2004@gmail.com>
2021-03-19 17:44:09 -0400

    cut out the tuning algorithm and massively simplify

--- graspologic/embed/case.py
+++ graspologic/embed/case.py
@@ -35,29 +35,9 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
 
         alpha : float, optional (default = None)
             Tuning parameter to use. Not used if embedding_alg == cca:
-                -  None: tune the alpha-value by minimizing the k-means objective function.
-                         Since this involves running k-means over a parameter space, this
-                         will result in a slower algorithm in exchange for likely better
-                         clustering.
-                - float: use a particular alpha-value. Results in a much faster algorithm
-                         (since we are not tuning with kmeans) in exchange for potentially
-                         suboptimal clustering results.
-                -    -1: Default to the ratio of the leading eigenvector of the Laplacian
-                         to the leading eigenvector of the covariate matrix. This will
-                         result in suboptimal clustering in exchange for increased
-                         clustering speed.
-
-        tuning_runs : int, optional (default = 100)
-            If tuning alpha with k-means, this parameter determines the number of times
-            k-means is run. Higher values are more computationally expensive in exchange
-            for a finer-grained search of the parameter space (and better embedding).
-
-        n_jobs : int, optional (default = -1)
-            The number of parallel threads to use in K-means when calculating alpha.
-            `-1` means using all processors.
-
-        verbose : int, optional (default = 0)
-            Verbosity mode.
+                -  None: Default to the ratio of the leading eigenvector of the Laplacian
+                         to the leading eigenvector of the covariate matrix.
+                - float: Use a particular alpha-value.
 
         n_elbows : int, optional, default: 2
             If `n_components=None`, then compute the optimal embedding dimension using
@@ -84,9 +64,6 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
         n_components=None,
         embedding_alg="assortative",
         alpha=None,
-        tuning_runs=20,
-        n_jobs=-1,
-        verbose=0,
         n_elbows=2,
         check_lcc=False,
         concat=False,
@@ -98,25 +75,18 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
             check_lcc=check_lcc,
             concat=concat,
         )
-        if not isinstance(embedding_alg, str):
-            raise TypeError("Embedding algorithm must be a string")
+
         if embedding_alg not in {"assortative", "non-assortative", "cca"}:
             msg = "embedding_alg must be in {assortative, non-assortative, cca}."
             raise ValueError(msg)
         self.embedding_alg = embedding_alg
 
-        if not ((alpha is None) or alpha == -1 or isinstance(alpha, float)):
-            msg = "alpha must be in {None, float, -1}."
+        if not ((alpha is None) or isinstance(alpha, float)):
+            msg = "alpha must be in {None, float}."
             raise TypeError(msg)
 
-        if n_jobs is None:
-            n_jobs = -1
-
-        self.n_jobs = n_jobs
         self.alpha = alpha
-        self.tuning_runs = tuning_runs
-        self.verbose = verbose
-        self.latent_right_ = None  # doesn't work for directed graphs atm
+        self.latent_right_ = None
         self.is_fitted_ = False
 
     def fit(self, graph, covariates, y=None, labels=None):
@@ -156,148 +126,55 @@ class CovariateAssistedEmbedding(BaseSpectralEmbed):
         # center and scale covariates to unit norm
         covariates = normalize(covariates, axis=0)
 
-        # save necessary params  # TODO: do this without saving potentially huge objects into `self`
-        self._L = to_laplacian(A, form="R-DAD")
-        self._R = np.shape(covariates)[1]
-        self._X = covariates.copy()
+        # save necessary params
+        L = to_laplacian(A, form="R-DAD")
+        X = covariates.copy()
 
         # change params based on tuning algorithm
         if self.embedding_alg == "cca":
-            self._LL = self._L @ self._X
-            self._reduce_dim(self._LL)
-            self.is_fitted_ = True
-            return self
+            LL = L @ X
+            XXt = 0
         elif self.embedding_alg == "assortative":
-            self._LL = self._L
-            self._XXt = self._X @ self._X.T
-            self.alpha_ = self._get_tuning_parameter()
+            LL = L.copy()
+            XXt = X @ X.T
         elif self.embedding_alg == "non-assortative":
-            self._LL = self._L @ self._L
-            self._XXt = self._X @ self._X.T
-            self.alpha_ = self._get_tuning_parameter()
+            LL = L @ L
+            XXt = X @ X.T
 
-        self.latent_left_ = _embed(
-            self.alpha_, self._LL, self._XXt, n_clusters=self.n_components
-        )
+        alpha_ = self._get_tuning_parameter(LL, XXt)
+        L_ = (LL + alpha_ * (XXt)).astype(float)
+        self._reduce_dim(L_)
+
         self.is_fitted_ = True
         return self
 
-    def _get_tuning_parameter(self):
+    def _get_tuning_parameter(self, LL, XXt):
         """
-        Find an alpha within a range which optimizes the k-means objective function on
-        our embedding.
+        Find the alpha which causes the leading eigenspace of LL and XXt to be the same.
 
         Parameters
         ----------
         LL : array
-            The squared regularized graph Laplacian
+            The regularized graph Laplacian (assortative)
+            The squared regularized graph Laplacian (non-assortative)
         XXt : array
             X@X.T, where X is the covariate matrix.
 
         Returns
         -------
         alpha : float
-            Tuning parameter which normalizes LL and XXt.
+            Tuning parameter which normalizes the leading eigenspace of LL and XXt.
         """
         # setup
-        if isinstance(self.alpha, (int, float)) and self.alpha != -1:
+        if self.alpha is not None:
             return self.alpha
-        n_clusters = self.n_components  # number of clusters
-        n_cov = self._R  # number of covariates
-
-        # grab eigenvalues
-        X_components = np.min([n_cov, n_clusters]) + 1
-        _, X_eigvals, _ = randomized_svd(self._X, n_components=X_components)
-        L_eigvals = np.flip(eigsh(self._L, k=n_clusters + 1, return_eigenvectors=False))
-        if self.embedding_alg == "non-assortative":
-            L_eigvals = L_eigvals ** 2
 
         # calculate bounds
-        L_top = L_eigvals[0]
-        X_top = X_eigvals[0]
-        amin = (L_eigvals[n_clusters - 1] - L_eigvals[n_clusters]) / X_top ** 2
-        if n_cov > n_clusters:
-            amax = L_top / (X_eigvals[n_clusters - 1] ** 2 - X_eigvals[n_clusters] ** 2)
-        else:
-            amax = L_top / X_eigvals[n_cov - 1] ** 2
+        (L_top,) = eigsh(LL, return_eigenvectors=False, k=1)
+        (X_top,) = eigsh(XXt, return_eigenvectors=False, k=1)
 
-        if self.alpha == -1:
-            # just use the ratio of the leading eigenvalues for the
-            # tuning parameter, or the closest value in its possible range.
-            alpha = np.float(L_top / X_top)
-            if amin <= alpha <= amax:
-                return alpha
-            elif alpha < amin:
-                return amin
-            elif alpha > amax:
-                return amax
-
-        # run kmeans clustering and set alpha to the value
-        # which minimizes clustering intertia
-        alpha_range = np.linspace(amin, amax, num=self.tuning_runs)
-        inertia_trials = (
-            delayed(_cluster)(alpha, LL=self._LL, XXt=self._XXt, n_clusters=n_clusters)
-            for alpha in alpha_range
-        )
-        inertias = dict(
-            Parallel(n_jobs=self.n_jobs, verbose=self.verbose)(inertia_trials)
-        )
-        alpha = min(inertias, key=inertias.get)
+        # just use the ratio of the leading eigenvalues for the
+        # tuning parameter, or the closest value in its possible range.
+        alpha = np.float(L_top / X_top)
 
         return alpha
-
-
-def _cluster(alpha, LL, XXt, *, n_clusters):
-    """
-    Fit and cluster the CASE matrix.
-
-    Parameters
-    ----------
-    alpha : float
-        Tuning parameter
-    LL : np.ndarray
-        Laplacian matrix
-    XXt : np.ndarray
-        XX^T for a covariate matrix X
-    n_clusters : int
-        Number of clusters in our data
-
-    Returns
-    -------
-    tuple
-        Returns the alpha value and the k-means inertia for the clustering on that
-        alpha value.
-    """
-    latents = _embed(alpha, LL=LL, XXt=XXt, n_clusters=n_clusters)
-    kmeans = KMeans(n_clusters=n_clusters)
-    kmeans.fit(latents)
-    return alpha, kmeans.inertia_
-
-
-def _embed(alpha, LL, XXt, *, n_clusters):
-    """
-    Embed the CASE matrix.
-
-    Parameters
-    ----------
-    alpha : float
-        Tuning parameter
-    LL : np.ndarray
-        Laplacian matrix
-    XXt : np.ndarray
-        XX^T for a covariate matrix X
-    n_clusters : int
-        Number of clusters in our data
-
-    Returns
-    -------
-    np.ndarray
-        Latent position matrix (e.g., eigenvectors) for our embedding.
-    """
-    L_ = (LL + alpha * (XXt)).astype(float)
-    _, vecs = eigsh(L_, k=n_clusters)
-
-    # descending order
-    vecs = np.flip(vecs, axis=1)
-
-    return vecs
