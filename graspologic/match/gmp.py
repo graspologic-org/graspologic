a6b6d58bbde8115e84d23507a3a8e5fc828572f4
Ali Saad-Eldin <ali.saadeldin11@gmail.com>
2021-03-19 17:38:07 -0400

    Update gmp.py

--- graspologic/match/gmp.py
+++ graspologic/match/gmp.py
@@ -177,8 +177,8 @@ class GraphMatch(BaseEstimator):
 
         seeds_B : 1d-array, shape (m , 1) where m <= number of nodes (default = [])
             An array where each entry is an index of a node in `B` The elements of
-            `seeds_A` and `seeds_B` are vertices which are known to be matched, that is,
-            `seeds_A[i]` is matched to vertex `seeds_B[i]`.
+            ``seeds_A`` and ``seeds_B`` are vertices which are known to be matched, that is,
+            ``seeds_A[i]`` is matched to vertex ``seeds_B[i]``.
         S : 2d-array, square
             A square similarity matrix. Should be same shape as ``A`` and ``B``.
 
