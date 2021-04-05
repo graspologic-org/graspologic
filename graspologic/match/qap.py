4d94a49e80808ef5d86918251a2a9d2d6977d3dd
Benjamin Pedigo <benjamindpedigo@gmail.com>
2021-04-05 09:56:25 -0400

    Update graspologic/match/qap.py

--- graspologic/match/qap.py
+++ graspologic/match/qap.py
@@ -279,7 +279,7 @@ def _quadratic_assignment_faq(
         ``partial_match`` is an array of size ``(m , 2)``, where ``m`` is
         not greater than the number of nodes, :math:`n`.
     S : 2d-array, square
-        A square similarity matrix. Should be same shape as ``A`` and ``B``.   
+        A similarity matrix. Should be same shape as ``A`` and ``B``.   
         Note: the scale of `S` may effect the weight placed on the term 
         :math:`\\text{trace}(S^T P)` relative to :math:`\\text{trace}(A^T PBP^T)` 
         during the optimization process.
