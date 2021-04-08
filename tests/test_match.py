71c44ac98d8c8c3ad80aa5cdb2c4b67fdc570c13
Kelly Marchisio <kellymarchisio@users.noreply.github.com>
2021-04-08 17:38:00 -0400

    add unit tests for outlier handling in qap

--- tests/test_match.py
+++ tests/test_match.py
@@ -141,6 +141,24 @@ class TestGMP:
         score = chr12c.score_
         assert 11156 == score
 
+        W1 = np.array(range(n))
+        W2 = [pi[z] for z in W1]
+        chr12c = self.barycenter.fit(A, B, W1, W2)
+        score = chr12c.score_
+        assert 11156 == score
+
+        W1 = np.array(range(n))
+        W2 = [pi[z] for z in W1]
+        # Shuffle seed pairs.
+        pairs = [i for i in zip(W1, W2)]
+        random.shuffle(pairs)
+        W1 = list(list(zip(*pairs))[0])
+        W2 = list(list(zip(*pairs))[1])
+        chr12c = self.barycenter.fit(A, B, W1, W2)
+        score = chr12c.score_
+        assert 11156 == score
+
+
     def test_rand_SGM(self):
         A, B = self._get_AB()
         chr12c = self.rand.fit(A, B)
