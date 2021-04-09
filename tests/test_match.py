576ac359ecbe33fdf5942e592aa67e6792a813fc
Kelly Marchisio <kellymarchisio@users.noreply.github.com>
2021-04-09 12:30:56 -0400

    update unit tests for outlier handling in qap

--- tests/test_match.py
+++ tests/test_match.py
@@ -142,22 +142,18 @@ class TestGMP:
         assert 11156 == score
 
         W1 = np.array(range(n))
+        W2 = pi
+        chr12c = self.barycenter.fit(A, B, W1, W2)
+        score = chr12c.score_
+        assert np.array_equal(chr12c.perm_inds_, pi)
+        assert 11156 == score
+
+        W1 = np.random.permutation(n)
         W2 = [pi[z] for z in W1]
         chr12c = self.barycenter.fit(A, B, W1, W2)
         score = chr12c.score_
+        assert np.array_equal(chr12c.perm_inds_, pi)
         assert 11156 == score
-
-        W1 = np.array(range(n))
-        W2 = [pi[z] for z in W1]
-        # Shuffle seed pairs.
-        pairs = [i for i in zip(W1, W2)]
-        random.shuffle(pairs)
-        W1 = list(list(zip(*pairs))[0])
-        W2 = list(list(zip(*pairs))[1])
-        chr12c = self.barycenter.fit(A, B, W1, W2)
-        score = chr12c.score_
-        assert 11156 == score
-
 
     def test_rand_SGM(self):
         A, B = self._get_AB()
