508b5441c97633e12587ce2b05319d773aff4134
Nick <nick@caurvina.com>
2021-04-20 14:51:54 -0700

    Allow arbitrary types of node ids for node2vec (#773)
    * Rename node ids to strings in n2v implementation
    
    * Add test for layout umap
    
    * Add test for layout umap with integer node ids
    
    * Fix mysterious whitespace issue
    
    * Fix mysterious whitespace issue
    
    * Fix mysterious whitespace issue
    
    * Appease linter
    
    * Run black linting
    
    * Remove superfluous n2v embedding shape test
    
    * Remove to_weighted_edgelist from utils init
    
    * Add directed v undirected test for n2v
    
    * Modify n2v tests to compare against expected labels
    
    * Modify n2v return type to be lists instead of np.array
    
    * Modify n2v return type to be lists instead of np.array
    
    * Run linter over n2v tests
    
    * n2v -> np.array, list signature
    
    * Actually finalize n2v api

--- tests/layouts/test_auto.py
+++ tests/layouts/test_auto.py
@@ -2,8 +2,10 @@
 # Licensed under the MIT License.
 
 import unittest
+import networkx as nx
 import numpy
-from graspologic.layouts.auto import _get_bounds
+
+from graspologic.layouts.auto import _get_bounds, layout_umap
 
 
 class TestAuto(unittest.TestCase):
@@ -14,6 +16,34 @@ class TestAuto(unittest.TestCase):
         self.assertEqual(-20, miny)
         self.assertEqual(10, maxx)
         self.assertEqual(5, maxy)
+
+    def test_layout_umap_string_node_ids(self):
+        graph = nx.florentine_families_graph()
+
+        for s, t in graph.edges():
+            graph.add_edge(s, t, weight=1)
+
+        _, node_positions = layout_umap(graph=graph)
+
+        self.assertEqual(len(node_positions), len(graph.nodes()))
+
+    def test_layout_umap_int_node_ids(self):
+        graph = nx.florentine_families_graph()
+        graph_int_node_ids = nx.Graph()
+        ids_as_ints = dict()
+
+        for s, t in graph.edges():
+            if s not in ids_as_ints:
+                ids_as_ints[s] = int(len(ids_as_ints.keys()))
+
+            if t not in ids_as_ints:
+                ids_as_ints[t] = int(len(ids_as_ints.keys()))
+
+            graph_int_node_ids.add_edge(ids_as_ints[s], ids_as_ints[t], weight=1)
+
+        _, node_positions = layout_umap(graph=graph_int_node_ids)
+
+        self.assertEqual(len(node_positions), len(graph.nodes()))
 
 
 if __name__ == "__main__":
