8e7ee76153b16dba580ba18ac746218e879cb3f3
Daniel Borders <danielborders@MacBook-Pro-2.local>
2021-04-20 11:48:25 -0400

    fixed embedding problem

--- graspologic/embed/lse.py
+++ graspologic/embed/lse.py
@@ -238,7 +238,7 @@ class LaplacianSpectralEmbed(BaseSpectralEmbed):
         self._reduce_dim(L_norm)
 
         # for out-of-sample
-        inv_eigs = np.diag(1 / self.singular_values_)
+        inv_eigs = np.diag(1 / self.singular_values_)/(self.n_features_in_/self.n_components)
         self._pinv_left = self.latent_left_ @ inv_eigs
         if self.latent_right_ is not None:
             self._pinv_right = self.latent_right_ @ inv_eigs
