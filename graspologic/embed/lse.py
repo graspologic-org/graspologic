6ced01ec8ddd4e8c417fcd3a9f72b66f12ad39be
Daniel Borders <danielborders@MacBook-Pro-4.local>
2021-05-04 18:33:46 -0400

    updated to fix minor error in testing

--- graspologic/embed/lse.py
+++ graspologic/embed/lse.py
@@ -184,18 +184,11 @@ class LaplacianSpectralEmbed(BaseSpectralEmbed):
             if X.ndim == 1:
                 return X @ self._pinv_left / np.sum(X)
             else:
-                return np.transpose(
-                    np.transpose(X @ self._pinv_left) / np.sum(X, axis=1)
-                )
+                return ((X @ self._pinv_left).T / np.sum(X, axis=1)).T
         elif directed:
             if X[0].ndim == 1:
-                return (
-                    X[1] @ self._pinv_right / np.sum(X[1], axis=1),
-                    X[0] @ self._pinv_left / np.sum(X[0], axis=1),
-                )
+                return X[1] @ self._pinv_right / np.sum(X[1]),\
+                       X[0] @ self._pinv_left / np.sum(X[0])
             else:
-                return np.transpose(
-                    np.transpose(X[1] @ self._pinv_right) / np.sum(X[1], axis=1)
-                ), np.transpose(
-                    np.transpose(X[0] @ self._pinv_left) / np.sum(X[0], axis=1)
-                )
+                return ((X[1] @ self._pinv_right).T / np.sum(X[1], axis=1)).T, \
+                    ((X[0] @ self._pinv_left).T / np.sum(X[0], axis=1)).T
