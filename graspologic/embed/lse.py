591294b0278d020e61e588aa00447716d86767e4
Daniel Borders <danielborders@MacBook-Pro-4.local>
2021-05-11 14:00:57 -0400

    fixed minor error

--- graspologic/embed/lse.py
+++ graspologic/embed/lse.py
@@ -181,10 +181,13 @@ class LaplacianSpectralEmbed(BaseSpectralEmbed):
 
             return ((X @ self._pinv_left).T / np.sum(X, axis=1)).T
         elif directed:
-            if X[0].ndim == 1:
-                X[0] = np.expand_dims(X[0], axis=0)
-                X[1] = np.expand_dims(X[1], axis=0)
+            X_0 = X[0]
+            X_1 = X[1]
 
-            return ((X[1] @ self._pinv_right).T / np.sum(X[1], axis=1)).T, (
-                (X[0] @ self._pinv_left).T / np.sum(X[0], axis=1)
+            if X[0].ndim == 1:
+                X_0 = np.expand_dims(X_0, axis=0)
+                X_1 = np.expand_dims(X_1, axis=0)
+
+            return ((X_1 @ self._pinv_right).T / np.sum(X_1, axis=1)).T, (
+                (X_0 @ self._pinv_left).T / np.sum(X_0, axis=1)
             ).T
