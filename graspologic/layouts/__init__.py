eab1d27cbbeb34a001b7034a742ac27e5a5d312f
dlee0156 <d0156lee@gmail.com>
2021-08-23 17:48:50 -0400

    Update __init__.py

--- graspologic/layouts/__init__.py
+++ graspologic/layouts/__init__.py
@@ -2,18 +2,28 @@
 # Licensed under the MIT license.
 
 
+from .auto import (
+    embed_for_layout,
+    get_node_positions,
+    get_partitions,
+    preprocess_for_layout,
+    reduce_dim_for_layout,
+)
 from .classes import NodePosition
 from .colors import categorical_colors, sequential_colors
 from .render import save_graph, show_graph
 
-from .auto import layout_tsne, layout_umap  # isort:skip
+# isort:skip
 
 __all__ = [
     "NodePosition",
     "categorical_colors",
     "sequential_colors",
-    "layout_tsne",
-    "layout_umap",
+    "preprocess_for_layout",
+    "embed_for_layout",
+    "reduce_dim_for_layout",
+    "get_partitions",
+    "get_node_positions",
     "save_graph",
     "show_graph",
 ]
