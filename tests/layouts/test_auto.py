489501bf3e88b2fc4486c733905e145a5c0e594a
dlee0156 <d0156lee@gmail.com>
2021-08-24 13:33:42 -0400

    Update test_auto.py

--- tests/layouts/test_auto.py
+++ tests/layouts/test_auto.py
@@ -6,7 +6,13 @@ import unittest
 import networkx as nx
 import numpy
 
-from graspologic.layouts.auto import _get_bounds, layout_umap
+from graspologic.layouts.auto import (
+    _get_bounds,
+    embed_for_layout,
+    get_node_positions,
+    preprocess_for_layout,
+    reduce_dim_for_layout,
+)
 
 
 class TestAuto(unittest.TestCase):
@@ -18,17 +24,22 @@ class TestAuto(unittest.TestCase):
         self.assertEqual(10, maxx)
         self.assertEqual(5, maxy)
 
-    def test_layout_umap_string_node_ids(self):
+    def test_layout_string_node_ids(self):
         graph = nx.florentine_families_graph()
 
         for s, t in graph.edges():
             graph.add_edge(s, t, weight=1)
 
-        _, node_positions = layout_umap(graph=graph)
+        lcc_graph = preprocess_for_layout(graph=graph)
+        tensors, labels = embed_for_layout(graph=lcc_graph)
+        points_2d = reduce_dim_for_layout(embedding=tensors)
+        node_positions = get_node_positions(
+            graph=lcc_graph, points_2d=points_2d, labels=labels
+        )
 
         self.assertEqual(len(node_positions), len(graph.nodes()))
 
-    def test_layout_umap_int_node_ids(self):
+    def test_layout_int_node_ids(self):
         graph = nx.florentine_families_graph()
         graph_int_node_ids = nx.Graph()
         ids_as_ints = dict()
@@ -42,24 +53,39 @@ class TestAuto(unittest.TestCase):
 
             graph_int_node_ids.add_edge(ids_as_ints[s], ids_as_ints[t], weight=1)
 
-        _, node_positions = layout_umap(graph=graph_int_node_ids)
+        lcc_graph = preprocess_for_layout(graph=graph_int_node_ids)
+        tensors, labels = embed_for_layout(graph=lcc_graph)
+        points_2d = reduce_dim_for_layout(embedding=tensors)
+        node_positions = get_node_positions(
+            graph=lcc_graph, points_2d=points_2d, labels=labels
+        )
 
         self.assertEqual(len(node_positions), len(graph.nodes()))
 
-    def test_layout_umap_directed_weighted(self):
+    def test_layout_directed_weighted(self):
         graph = nx.erdos_renyi_graph(10, 0.7, directed=True)
 
         for s, t in graph.edges():
             graph.edges[s, t]["weight"] = numpy.random.randint(0, 10)
 
-        _, node_positions = layout_umap(graph=graph)
+        lcc_graph = preprocess_for_layout(graph=graph)
+        tensors, labels = embed_for_layout(graph=lcc_graph)
+        points_2d = reduce_dim_for_layout(embedding=tensors)
+        node_positions = get_node_positions(
+            graph=lcc_graph, points_2d=points_2d, labels=labels
+        )
 
         self.assertEqual(len(node_positions), len(graph.nodes()))
 
     def test_layout_umap_directed_unweighted(self):
         graph = nx.erdos_renyi_graph(10, 0.7, directed=True)
 
-        _, node_positions = layout_umap(graph=graph)
+        lcc_graph = preprocess_for_layout(graph=graph)
+        tensors, labels = embed_for_layout(graph=lcc_graph)
+        points_2d = reduce_dim_for_layout(embedding=tensors)
+        node_positions = get_node_positions(
+            graph=lcc_graph, points_2d=points_2d, labels=labels
+        )
 
         self.assertEqual(len(node_positions), len(graph.nodes()))
 
