2faf2660e532e3b6d76f81f6b47b9da301aa3af1
Patrick Bourke <pbourke@microsoft.com>
2021-09-22 15:27:59 -0700

    Type hints for graspologic/inference

--- graspologic/inference/latent_position_test.py
+++ graspologic/inference/latent_position_test.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT License.
 
 from collections import namedtuple
+from typing import cast, Literal, Optional, Tuple
 
 import numpy as np
 from joblib import Parallel, delayed
@@ -9,20 +10,24 @@ from joblib import Parallel, delayed
 from ..align import OrthogonalProcrustes
 from ..embed import AdjacencySpectralEmbed, OmnibusEmbed, select_dimension
 from ..simulations import rdpg
+from ..types import GraphRepresentation
 from ..utils import import_graph, is_symmetric
 
 lpt_result = namedtuple("lpt_result", ("p_value", "sample_T_statistic", "misc_stats"))
 
+LptEmbeddingType = Literal["ase", "omnibus"]
+LptHypothesisTestType = Literal["rotation", "scalar-rotation", "diagonal-rotation"]
+
 
 def latent_position_test(
-    A1,
-    A2,
-    embedding="ase",
-    n_components=None,
-    test_case="rotation",
-    n_bootstraps=500,
-    workers=1,
-):
+    A1: GraphRepresentation,
+    A2: GraphRepresentation,
+    embedding: LptEmbeddingType = "ase",
+    n_components: Optional[int] = None,
+    test_case: LptHypothesisTestType = "rotation",
+    n_bootstraps: int = 500,
+    workers: int = 1,
+) -> lpt_result:
     r"""
     Two-sample hypothesis test for the problem of determining whether two random
     dot product graphs have the same latent positions.
@@ -143,23 +148,26 @@ def latent_position_test(
         raise NotImplementedError()  # TODO asymmetric case
     if A1.shape != A2.shape:
         raise ValueError("Input matrices do not have matching dimensions")
+    num_components: int
     if n_components is None:
         # get the last elbow from ZG for each and take the maximum
         num_dims1 = select_dimension(A1)[0][-1]
         num_dims2 = select_dimension(A2)[0][-1]
-        n_components = max(num_dims1, num_dims2)
-    X_hats = _embed(A1, A2, embedding, n_components)
+        num_components = max(num_dims1, num_dims2)
+    else:
+        num_components = n_components
+    X_hats = _embed(A1, A2, embedding, num_components)
     sample_T_statistic = _difference_norm(X_hats[0], X_hats[1], embedding, test_case)
 
     # Compute null distributions
     null_distribution_1 = Parallel(n_jobs=workers)(
-        delayed(_bootstrap)(X_hats[0], embedding, n_components, n_bootstraps, test_case)
+        delayed(_bootstrap)(X_hats[0], embedding, num_components, n_bootstraps, test_case)
         for _ in range(n_bootstraps)
     )
     null_distribution_1 = np.array(null_distribution_1)
 
     null_distribution_2 = Parallel(n_jobs=workers)(
-        delayed(_bootstrap)(X_hats[1], embedding, n_components, n_bootstraps, test_case)
+        delayed(_bootstrap)(X_hats[1], embedding, num_components, n_bootstraps, test_case)
         for _ in range(n_bootstraps)
     )
     null_distribution_2 = np.array(null_distribution_2)
@@ -185,8 +193,8 @@ def latent_position_test(
 
 
 def _bootstrap(
-    X_hat, embedding, n_components, n_bootstraps, test_case, rescale=False, loops=False
-):
+    X_hat: np.ndarray, embedding: LptEmbeddingType, n_components: int, n_bootstraps: int, test_case: LptHypothesisTestType, rescale: bool = False, loops: bool = False
+) -> float:
     A1_simulated = rdpg(X_hat, rescale=rescale, loops=loops)
     A2_simulated = rdpg(X_hat, rescale=rescale, loops=loops)
     X1_hat_simulated, X2_hat_simulated = _embed(
@@ -198,7 +206,7 @@ def _bootstrap(
     return t_bootstrap
 
 
-def _difference_norm(X1, X2, embedding, test_case):
+def _difference_norm(X1: np.ndarray, X2: np.ndarray, embedding: LptEmbeddingType, test_case: LptHypothesisTestType) -> float:
     if embedding in ["ase"]:
         if test_case == "rotation":
             pass
@@ -217,14 +225,16 @@ def _difference_norm(X1, X2, embedding, test_case):
     return np.linalg.norm(X1 - X2)
 
 
-def _embed(A1, A2, embedding, n_components, check_lcc=True):
+def _embed(A1: np.ndarray, A2: np.ndarray, embedding: LptEmbeddingType, n_components: int, check_lcc: bool = True) -> Tuple[np.ndarray, np.ndarray]:
+    X1_hat: np.ndarray
+    X2_hat: np.ndarray
     if embedding == "ase":
-        X1_hat = AdjacencySpectralEmbed(
+        X1_hat = cast(np.ndarray, AdjacencySpectralEmbed(
             n_components=n_components, check_lcc=check_lcc
-        ).fit_transform(A1)
-        X2_hat = AdjacencySpectralEmbed(
+        ).fit_transform(A1))
+        X2_hat = cast(np.ndarray, AdjacencySpectralEmbed(
             n_components=n_components, check_lcc=check_lcc
-        ).fit_transform(A2)
+        ).fit_transform(A2))
     elif embedding == "omnibus":
         X_hat_compound = OmnibusEmbed(
             n_components=n_components, check_lcc=check_lcc
