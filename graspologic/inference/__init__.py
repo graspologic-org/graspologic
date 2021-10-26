97e56624e9fc452516a5b178a670877ec02de725
Kareef Ullah <kareef928@gmail.com>
2021-10-26 13:35:30 -0400

    Revert "fix add_variance"
    This reverts commit 47b0c4d2eb76e4e0a03ed02916f1711722bf2b4f.

--- graspologic/inference/__init__.py
+++ graspologic/inference/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation and contributors.
 # Licensed under the MIT License.
 
-from .latent_distribution_test import latent_distribution_test, _sample_modified_ase
+from .latent_distribution_test import latent_distribution_test
 from .latent_position_test import latent_position_test
 
-__all__ = ["latent_position_test", "latent_distribution_test", "_sample_modified_ase"]
+__all__ = ["latent_position_test", "latent_distribution_test"]
