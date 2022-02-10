fc0ef9c1d05fec439f25a1f95ada963a2db8e0e4
Dax Pryce <daxpryce@microsoft.com>
2022-02-10 11:03:33 -0800

    This hyppo fix is getting out of control. We have now officially deprecated Python 3.6, no longer need to limit POT, require the latest beartype in all the land just to avoid deprecation warnings if we use the old Exceptions instead of the Violations, and I did a lot of packaging junk to ensure it won't even try to install on Python 3.6 now

--- graspologic/pipeline/embed/adjacency_spectral_embedding.py
+++ graspologic/pipeline/embed/adjacency_spectral_embedding.py
@@ -106,7 +106,7 @@ def adjacency_spectral_embedding(
 
     Raises
     ------
-    beartype.roar.BeartypeCallHintPepParamException if parameters do not match type hints
+    beartype.roar.BeartypeCallHintParamViolation if parameters do not match type hints
     ValueError if values are not within appropriate ranges or allowed values
 
     See Also
