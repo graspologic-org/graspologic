2b1c0fa7c6b85a2c82c53e9e60775926e17b2987
Eric W. Bridgeford <ebridge2@jhu.edu>
2022-03-14 04:02:45 -0400

    updated with black

--- graspologic/simulations/__init__.py
+++ graspologic/simulations/__init__.py
@@ -1,7 +1,16 @@
 # Copyright (c) Microsoft Corporation and contributors.
 # Licensed under the MIT License.
 
-from .simulations import er_nm, er_np, mmsbm, p_from_latent, rdpg, sample_edges, sbm, siem
+from .simulations import (
+    er_nm,
+    er_np,
+    mmsbm,
+    p_from_latent,
+    rdpg,
+    sample_edges,
+    sbm,
+    siem,
+)
 from .simulations_corr import er_corr, sample_edges_corr, sbm_corr
 
 from .rdpg_corr import rdpg_corr  # isort:skip
