4cf39565d76225d1081bc30c63d67f34718db58d
Kareef Ullah <kareef928@gmail.com>
2022-03-29 14:39:10 -0400

    make edge_list an instance variable

--- graspologic/models/edge_swaps.py
+++ graspologic/models/edge_swaps.py
@@ -1,37 +1,36 @@
 from typing import Union
 
+import warnings
 import numba as nb
 import numpy as np
 from scipy.sparse import lil_matrix
 
 from graspologic.types import Tuple
 
+warnings.filterwarnings("ignore")
+
 
 class EdgeSwap:
     def __init__(self, adjacency: Union[np.ndarray, lil_matrix]):
         self.adjacency = adjacency
+        self.edge_list = self._do_setup()
 
     def _do_setup(self) -> np.ndarray:
         row_inds, col_inds = np.nonzero(self.adjacency)
         edge_list = np.stack((row_inds, col_inds)).T
         return edge_list
 
-    def _true_edge_swap(self, edge_list: np.ndarray) -> np.ndarray:
-        self.adjacency, edge_list = self._edge_swap(self.adjacency, edge_list)
-        return edge_list
-
     @staticmethod
     @nb.jit
     def _edge_swap(
-        adjacency: Union[np.ndarray, lil_matrix], edge_list: np.ndarray
+        adjacency: Union[np.ndarray, lil_matrix], edge_list: np.ndarray, a: int = 1234
     ) -> Tuple[Union[np.ndarray, lil_matrix], np.ndarray]:
         # checks if there are at 2 edges in the graph
         if len(edge_list) < 2:
-            # print("graph has less than two edges")
             return adjacency, edge_list
 
         # choose two indices at random
-        np.random.seed(np.random.randint(12345))
+        np.random.seed(a)
         orig_inds = np.random.choice(len(edge_list), size=2, replace=False)
 
         u, v = edge_list[orig_inds[0]]
@@ -39,12 +38,10 @@ class EdgeSwap:
 
         # ensures no initial loops
         if u == v or x == y:
-            # print("initial loops")
             return adjacency, edge_list
 
         # ensures no loops after swap (must be swap on 4 distinct nodes)
         if u == x or v == y:
-            # print("loops after swap")
             return adjacency, edge_list
 
         # save edge values
@@ -55,12 +52,10 @@ class EdgeSwap:
 
         # ensures no initial multigraphs
         if w_uv > 1 or w_xy > 1:
-            # print("initial multigraph")
             return adjacency, edge_list
 
         # ensures no multigraphs after swap
         if w_ux >= 1 or w_vy >= 1:
-            # print("multigraph after swap")
             return adjacency, edge_list
 
         # perform the swap
@@ -76,11 +71,12 @@ class EdgeSwap:
         return adjacency, edge_list
 
     def _do_some_edge_swaps(
-        self, n_swaps: int = 1
+        self, n_swaps: int = 10
     ) -> Tuple[Union[np.ndarray, lil_matrix], np.ndarray]:
-        edge_list = self._do_setup()
 
         for swap in range(n_swaps):
-            edge_list = self._true_edge_swap(edge_list)
+            self.adjacency, self.edge_list = self._edge_swap(
+                self.adjacency, self.edge_list
+            )
 
-        return self.adjacency, edge_list
+        return self.adjacency, self.edge_list
