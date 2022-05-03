66ed1b047579309e844a2b88241ca64885a41bf7
Kareef Ullah <kareef928@gmail.com>
2022-05-03 14:04:30 -0400

    format

--- tests/test_models.py
+++ tests/test_models.py
@@ -606,13 +606,14 @@ class TestEdgeSwaps(unittest.TestCase):
         swapped_er, _ = Swapper.swap_edges(n_swaps=100)
         swapped_er_nx = nx.from_numpy_array(swapped_er)
         assert list(self.C.degree()) == list(swapped_er_nx.degree())
-    
+
     def test_scipy_edge_swap(self):
         Swapper = EdgeSwapper(self.B)
         swapped_csr, _ = Swapper.swap_edges(n_swaps=100)
         swapped_csr = swapped_csr.toarray()
         swapped_csr_nx = nx.from_numpy_array(swapped_csr)
         assert list(self.D.degree()) == list(swapped_csr_nx.degree())
+
 
 def hardy_weinberg(theta):
     """
