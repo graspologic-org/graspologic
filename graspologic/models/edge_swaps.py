8b52d7f3df585e4a987a1d401737d76cd2c98578
bdpedigo <benjamindpedigo@gmail.com>
2022-05-05 10:43:09 -0400

    try reworking to use JIT optionally as a function

--- graspologic/models/edge_swaps.py
+++ graspologic/models/edge_swaps.py
@@ -1,15 +1,13 @@
-import warnings
 from typing import Optional
 
 import numba as nb
 import numpy as np
 from beartype import beartype
-from numba.core.errors import NumbaWarning
-from scipy.sparse import SparseEfficiencyWarning, csr_matrix, lil_matrix
+from scipy.sparse import csr_matrix, lil_matrix
 
 from graspologic.preconditions import check_argument
 from graspologic.types import AdjacencyMatrix, Tuple
-from graspologic.utils import is_loopless, is_symmetric, is_unweighted, import_graph
+from graspologic.utils import import_graph, is_loopless, is_symmetric, is_unweighted
 
 
 # Code based on: https://github.com/joelnish/double-edge-swap-mcmc/blob/master/dbl_edge_mcmc.py
@@ -56,9 +54,14 @@ class EdgeSwapper:
 
         adjacency = import_graph(adjacency, copy=True)
 
-        # more efficient for manipulations which change sparsity structure
         if isinstance(adjacency, csr_matrix):
+            # more efficient for manipulations which change sparsity structure
             adjacency = lil_matrix(adjacency)
+            self._edge_swap_function = _edge_swap
+        else:
+            # for numpy input, use numba for JIT compilation
+            # NOTE: not convinced numba is helping much here, look into optimizing
+            self._edge_swap_function = nb.jit(_edge_swap)
 
         self.adjacency = adjacency
 
@@ -84,76 +87,6 @@ class EdgeSwapper:
         edge_list = np.stack((row_inds, col_inds)).T
         return edge_list
 
-    @staticmethod
-    @nb.jit
-    def _edge_swap(
-        adjacency: AdjacencyMatrix, edge_list: np.ndarray
-    ) -> Tuple[AdjacencyMatrix, np.ndarray]:
-        """
-        Performs the edge swap on the adjacency matrix. If adjacency is
-        np.ndarray, then nopython=True is used in numba, but if adjacency
-        is csr_matrix, then forceobj=True is used in numba
-
-        Parameters
-        ----------
-        adjacency : np.ndarray OR csr_matrix, shape (n_verts, n_verts)
-            The initial adjacency matrix in which edge swaps are performed on it
-
-        edge_list : np.ndarray, shape (n_verts, 2)
-            The corresponding edge_list of adjacency
-
-        Returns
-        -------
-        adjacency : np.ndarray OR csr_matrix, shape (n_verts, n_verts)
-            The adjancency matrix after an edge swap is performed on the graph
-
-        edge_list : np.ndarray (n_verts, 2)
-            The edge_list after an edge swap is perfomed on the graph
-        """
-
-        # choose two indices at random
-        orig_inds = np.random.choice(len(edge_list), size=2, replace=False)
-
-        u, v = edge_list[orig_inds[0]]
-
-        # two types of swap orientations for undirected graph
-        if np.random.rand() < 0.5:
-            x, y = edge_list[orig_inds[1]]
-        else:
-            y, x = edge_list[orig_inds[1]]
-
-        # ensures no initial loops
-        if u == v or x == y:
-            return adjacency, edge_list
-
-        # ensures no loops after swap (must be swap on 4 distinct nodes)
-        if u == x or v == y:
-            return adjacency, edge_list
-
-        # save edge values
-        w_ux = adjacency[u, x]
-        w_vy = adjacency[v, y]
-
-        # ensures no multigraphs after swap
-        if w_ux >= 1 or w_vy >= 1:
-            return adjacency, edge_list
-
-        # perform the swap
-        adjacency[u, v] = 0
-        adjacency[v, u] = 0
-        adjacency[x, y] = 0
-        adjacency[y, x] = 0
-
-        adjacency[u, x] = 1
-        adjacency[x, u] = 1
-        adjacency[v, y] = 1
-        adjacency[y, v] = 1
-
-        # update edge list
-        edge_list[orig_inds[0]] = [u, x]
-        edge_list[orig_inds[1]] = [v, y]
-        return adjacency, edge_list
-
     def swap_edges(
         self, n_swaps: int = 1, seed: Optional[int] = None
     ) -> Tuple[AdjacencyMatrix, np.ndarray]:
@@ -176,15 +109,82 @@ class EdgeSwapper:
         if seed is not None:
             np.random.randint(seed)
 
-        # with warnings.catch_warnings():
-        #     warnings.filterwarnings("ignore", category=NumbaWarning)
-        #     warnings.filterwarnings("ignore", category=SparseEfficiencyWarning)
         for _ in range(n_swaps):
-            self.adjacency, self.edge_list = self._edge_swap(
+            self.adjacency, self.edge_list = self._edge_swap_function(
                 self.adjacency, self.edge_list
             )
 
         adjacency = self.adjacency
         if isinstance(adjacency, csr_matrix):
             adjacency = csr_matrix(adjacency)
+
         return adjacency, self.edge_list
+
+
+def _edge_swap(
+    adjacency: AdjacencyMatrix, edge_list: np.ndarray
+) -> Tuple[AdjacencyMatrix, np.ndarray]:
+    """
+    Performs the edge swap on the adjacency matrix. If adjacency is
+    np.ndarray, then nopython=True is used in numba, but if adjacency
+    is csr_matrix, then forceobj=True is used in numba
+
+    Parameters
+    ----------
+    adjacency : np.ndarray OR csr_matrix, shape (n_verts, n_verts)
+        The initial adjacency matrix in which edge swaps are performed on it
+
+    edge_list : np.ndarray, shape (n_verts, 2)
+        The corresponding edge_list of adjacency
+
+    Returns
+    -------
+    adjacency : np.ndarray OR csr_matrix, shape (n_verts, n_verts)
+        The adjancency matrix after an edge swap is performed on the graph
+
+    edge_list : np.ndarray (n_verts, 2)
+        The edge_list after an edge swap is perfomed on the graph
+    """
+    # choose two indices at random
+    # NOTE: using np.random here for current numba compatibility
+    orig_inds = np.random.choice(len(edge_list), size=2, replace=False)
+
+    u, v = edge_list[orig_inds[0]]
+
+    # two types of swap orientations for undirected graph
+    if np.random.rand() < 0.5:
+        x, y = edge_list[orig_inds[1]]
+    else:
+        y, x = edge_list[orig_inds[1]]
+
+    # ensures no initial loops
+    if u == v or x == y:
+        return adjacency, edge_list
+
+    # ensures no loops after swap (must be swap on 4 distinct nodes)
+    if u == x or v == y:
+        return adjacency, edge_list
+
+    # save edge values
+    w_ux = adjacency[u, x]
+    w_vy = adjacency[v, y]
+
+    # ensures no multigraphs after swap
+    if w_ux >= 1 or w_vy >= 1:
+        return adjacency, edge_list
+
+    # perform the swap
+    adjacency[u, v] = 0
+    adjacency[v, u] = 0
+    adjacency[x, y] = 0
+    adjacency[y, x] = 0
+
+    adjacency[u, x] = 1
+    adjacency[x, u] = 1
+    adjacency[v, y] = 1
+    adjacency[y, v] = 1
+
+    # update edge list
+    edge_list[orig_inds[0]] = [u, x]
+    edge_list[orig_inds[1]] = [v, y]
+    return adjacency, edge_list
