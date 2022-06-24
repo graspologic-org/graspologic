4f1d263e4e7e4ba49374754ce8f2185034db5e41
bdpedigo <benjamindpedigo@gmail.com>
2022-06-24 11:49:27 -0400

    add types to wrapper

--- graspologic/match/wrappers.py
+++ graspologic/match/wrappers.py
@@ -6,8 +6,10 @@ from graspologic.types import AdjacencyMatrix, Dict, List, Tuple
 
 # Type aliases
 PaddingType = Literal["adopted", "naive"]
-InitMethodType = Literal["barycenter", "rand", "randomized"]
-RandomStateType = Optional[Union[int, np.random.RandomState, np.random.Generator]]
+InitMethodType = Union[float, Literal["barycenter", "random"]]
+RandomStateType = Optional[
+    Union[int, np.integer, np.random.RandomState, np.random.Generator]
+]
 ArrayLikeOfIndexes = Union[List[int], np.ndarray]
 MultilayerAdjacency = Union[List[AdjacencyMatrix], AdjacencyMatrix, np.ndarray]
 Scalar = Union[int, float, np.integer]
@@ -15,29 +17,26 @@ Int = Union[int, np.integer]
 
 
 def graph_match(
-    A,
-    B,
-    partial_match=None,
-    similarity=None,
-    AtoB=None,
-    BtoA=None,
-    init="barycenter",
-    n_init=1,
-    shuffle_input=True,
-    maximize=True,
-    padding="adopted",
-    n_jobs=None,
-    max_iter=30,
-    tol=0.01,
-    verbose=0,
-    random_state=None,
-    use_numba=True,
-    transport=False,
+    A: MultilayerAdjacency,
+    B: MultilayerAdjacency,
+    partial_match: Optional[np.ndarray] = None,
+    similarity: Optional[AdjacencyMatrix] = None,
+    AtoB: Optional[MultilayerAdjacency] = None,
+    BtoA: Optional[MultilayerAdjacency] = None,
+    init: InitMethodType = "barycenter",
+    n_init: Int = 1,
+    shuffle_input: bool = True,
+    maximize: bool = True,
+    padding: PaddingType = "adopted",
+    n_jobs: Optional[Int] = None,
+    maxiter: Int = 30,
+    tol: Scalar = 0.01,
+    verbose: Int = 0,
+    random_state: Optional[RandomStateType] = None,
+    numba: bool = True,
+    transport: bool = False,
     transport_regularizer: Scalar = 100,
     transport_tolerance: Scalar = 5e-2,
     transport_max_iter: Int = 1000,
 ) -> Tuple[np.ndarray, np.ndarray, Dict]:
     pass
-
-
-def soft_graph_match(A, B, ...)
