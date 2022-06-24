ad87bbf55ecdb21a5139a9b40f0bf9a6ff07e264
bdpedigo <benjamindpedigo@gmail.com>
2022-06-24 12:12:29 -0400

    add some type hints, remove _doubly_stochastic

--- graspologic/match/solver.py
+++ graspologic/match/solver.py
@@ -1,7 +1,9 @@
+from ast import Call
+from subprocess import call
 import time
 import warnings
 from functools import wraps
-from typing import Literal, Optional, Union
+from typing import Literal, Optional, Union, Callable
 
 import numpy as np
 from beartype import beartype
@@ -10,6 +12,7 @@ from ot import sinkhorn
 from scipy.optimize import linear_sum_assignment
 from scipy.sparse import csr_matrix
 from sklearn.base import BaseEstimator
+from sklearn.metrics import pair_confusion_matrix
 from sklearn.utils import check_random_state
 
 from graspologic.types import AdjacencyMatrix, List, Tuple
@@ -25,9 +28,9 @@ Scalar = Union[int, float, np.integer]
 Int = Union[int, np.integer]
 
 
-def parametrized(dec):
-    def layer(*args, **kwargs):
-        def repl(f):
+def parametrized(dec: Callable) -> Callable:
+    def layer(*args, **kwargs) -> Callable:  # type: ignore
+        def repl(f: Callable) -> Callable:
             return dec(f, *args, **kwargs)
 
         return repl
@@ -36,9 +39,9 @@ def parametrized(dec):
 
 
 @parametrized
-def write_status(f, msg, level):
+def write_status(f: Callable, msg: str, level: int) -> Callable:
     @wraps(f)
-    def wrap(*args, **kw):
+    def wrap(*args, **kw):  # type: ignore
         obj = args[0]
         verbose = obj.verbose
         if level <= verbose:
@@ -98,7 +101,6 @@ class GraphMatchSolver(BaseEstimator):
         use_numba: bool = True,
         transport_regularizer: Scalar = 100,
         transport_tolerance: Scalar = 5e-2,
-        transport_implementation: Literal["pot", "ds"] = "pot",
         transport_maxiter: Int = 1000,
     ):
         # TODO more input checking
@@ -113,7 +115,6 @@ class GraphMatchSolver(BaseEstimator):
         self.transport = transport
         self.transport_regularizer = transport_regularizer
         self.transport_tolerance = transport_tolerance
-        self.transport_implementation = transport_implementation
         self.transport_maxiter = transport_maxiter
 
         if maximize:
@@ -122,15 +123,15 @@ class GraphMatchSolver(BaseEstimator):
             self.obj_func_scalar = 1
 
         if partial_match is None:
-            partial_match = np.array([[], []]).astype(int).T
+            seeds = np.array([[], []]).astype(int).T
             self._seeded = False
         else:
             self._seeded = True
-        self.partial_match = partial_match
+            seeds = partial_match
 
         # TODO input validation
         # TODO seeds
-        # A, B, partial_match = _common_input_validation(A, B, partial_match)
+        # A, B, seeds = _common_input_validation(A, B, seeds)
 
         # convert everything to make sure they are 3D arrays (first dim is layer)
         A = _check_input_matrix(A)
@@ -140,7 +141,7 @@ class GraphMatchSolver(BaseEstimator):
         self.n_A = A[0].shape[0]
         self.n_B = B[0].shape[0]
         self.n_layers = len(A)
-        n_seeds = len(partial_match)
+        n_seeds = len(seeds)
         self.n_seeds = n_seeds
 
         # check for between-graph terms
@@ -162,14 +163,14 @@ class GraphMatchSolver(BaseEstimator):
 
         # set up so that seeds are first and we can grab subgraphs easily
         # TODO could also do this slightly more efficiently just w/ smart indexing?
-        # TODO I think this is kind making the assumption that the input partial_match
+        # TODO I think this is kind making the assumption that the input seeds
         # is sorted
-        sort_inds = np.argsort(partial_match[:, 0])
-        partial_match = partial_match[sort_inds]
-        nonseed_A = np.setdiff1d(range(A[0].shape[0]), partial_match[:, 0])
-        nonseed_B = np.setdiff1d(range(B[0].shape[0]), partial_match[:, 1])
-        perm_A = np.concatenate([partial_match[:, 0], nonseed_A])
-        perm_B = np.concatenate([partial_match[:, 1], nonseed_B])
+        sort_inds = np.argsort(seeds[:, 0])
+        seeds = seeds[sort_inds]
+        nonseed_A = np.setdiff1d(np.arange(A[0].shape[0]), seeds[:, 0])
+        nonseed_B = np.setdiff1d(range(B[0].shape[0]), seeds[:, 1])
+        perm_A = np.concatenate([seeds[:, 0], nonseed_A])
+        perm_B = np.concatenate([seeds[:, 1], nonseed_B])
         self.perm_A = perm_A
         self.perm_B = perm_B
         self._undo_perm_A = np.argsort(perm_A)
@@ -209,7 +210,7 @@ class GraphMatchSolver(BaseEstimator):
                 self._compute_gradient = _compute_gradient_numba
                 self._compute_coefficients = _compute_coefficients_numba
 
-    def solve(self):
+    def solve(self) -> None:
         self.n_iter = 0
         self.check_outlier_cases()
 
@@ -234,11 +235,11 @@ class GraphMatchSolver(BaseEstimator):
         self.finalize(P)
 
     # TODO
-    def check_outlier_cases(self):
+    def check_outlier_cases(self) -> None:
         pass
 
     @write_status("Initializing", 1)
-    def initialize(self):
+    def initialize(self) -> np.ndarray:
         if isinstance(self.init, float):
             n_unseed = self.n_unseed
             rng = self.rng
@@ -246,7 +247,8 @@ class GraphMatchSolver(BaseEstimator):
             # DO linear combo from barycenter
             K = rng.uniform(size=(n_unseed, n_unseed))
             # Sinkhorn balancing
-            K = _doubly_stochastic(K)
+            ones = np.ones(n_unseed)
+            K = sinkhorn(ones, ones, K, reg=0, numItermax=1000, stopThr=1e-3)
             P = J * self.init + K * (1 - self.init)  # TODO check how defined in paper
         elif isinstance(self.init, np.ndarray):
             raise NotImplementedError()
@@ -261,7 +263,7 @@ class GraphMatchSolver(BaseEstimator):
         return P
 
     @write_status("Computing constant terms", 2)
-    def compute_constant_terms(self):
+    def compute_constant_terms(self) -> None:
         self.constant_sum = np.zeros((self.n_layers, self.n_B, self.n_B))
         if self._seeded:
             n_layers = len(self.A)
@@ -274,22 +276,20 @@ class GraphMatchSolver(BaseEstimator):
                 contra.append(
                     self.AB_ns[i] @ self.BA_ns[i].T + self.BA_sn[i].T @ self.AB_sn[i]
                 )
-            ipsi = np.array(ipsi)
-            contra = np.array(contra)
-            self.ipsi_constant_sum = ipsi
-            self.contra_constant_sum = contra
-            self.constant_sum = ipsi + contra
+            self.ipsi_constant_sum = np.array(ipsi)
+            self.contra_constant_sum = np.array(contra)
+            self.constant_sum = self.ipsi_constant_sum + self.contra_constant_sum
         self.constant_sum += self.S
 
     @write_status("Computing gradient", 2)
-    def compute_gradient(self, P):
+    def compute_gradient(self, P: np.ndarray) -> np.ndarray:
         gradient = self._compute_gradient(
             P, self.A, self.B, self.AB, self.BA, self.constant_sum
         )
         return gradient
 
     @write_status("Solving assignment problem", 2)
-    def compute_step_direction(self, gradient):
+    def compute_step_direction(self, gradient: np.ndarray) -> np.ndarray:
         # [1] Algorithm 1 Line 4 - get direction Q by solving Eq. 8
         if self.transport:
             Q = self.linear_sum_transport(gradient)
@@ -298,7 +298,7 @@ class GraphMatchSolver(BaseEstimator):
             Q = np.eye(self.n_unseed)[permutation]
         return Q
 
-    def linear_sum_assignment(self, P):
+    def linear_sum_assignment(self, P: np.ndarray) -> np.ndarray:
         """This is a modified version of LAP which (in expectation) does not care
         about the order of the inputs. This matters because scipy LAP settles ties
         (which do come up) based on the ordering of the inputs. This can lead to
@@ -312,43 +312,37 @@ class GraphMatchSolver(BaseEstimator):
 
     def linear_sum_transport(
         self,
-        P,
-    ):
+        P: np.ndarray,
+    ) -> np.ndarray:
         maximize = self.maximize
         reg = self.transport_regularizer
 
         power = -1 if maximize else 1
         lamb = reg / np.max(np.abs(P))
-        if self.transport_implementation == "pot":
-            ones = np.ones(P.shape[0])
-            P_eps, log = sinkhorn(
-                ones,
-                ones,
-                P,
-                power / lamb,
-                stopThr=self.transport_tolerance,
-                numItermax=self.transport_maxiter,
-                log=True,
-                warn=False,
-            )
-            if log["niter"] == self.transport_maxiter - 1:
-                warnings.warn(
-                    "Sinkhorn-Knopp algorithm for solving linear sum transport "
-                    f"problem did not converge. The final error was {log['err'][-1]} "
-                    f"and the `transport_tolerance` was {self.transport_tolerance}. "
-                    "You may want to consider increasing "
-                    "`transport_regularizer`, increasing `transport_maxiter`, or this "
-                    "could be the result of `transport_tolerance` set too small."
-                )
-        elif self.transport_implementation == "ds":
-            P = np.exp(lamb * power * P)
-            P_eps = _doubly_stochastic(
-                P, self.transport_tolerance, self.transport_maxiter
+        ones = np.ones(P.shape[0])
+        P_eps, log = sinkhorn(
+            ones,
+            ones,
+            P,
+            power / lamb,
+            stopThr=self.transport_tolerance,
+            numItermax=self.transport_maxiter,
+            log=True,
+            warn=False,
+        )
+        if log["niter"] == self.transport_maxiter - 1:
+            warnings.warn(
+                "Sinkhorn-Knopp algorithm for solving linear sum transport "
+                f"problem did not converge. The final error was {log['err'][-1]} "
+                f"and the `transport_tolerance` was {self.transport_tolerance}. "
+                "You may want to consider increasing "
+                "`transport_regularizer`, increasing `transport_maxiter`, or this "
+                "could be the result of `transport_tolerance` set too small."
             )
         return P_eps
 
     @write_status("Computing step size", 2)
-    def compute_step_size(self, P, Q):
+    def compute_step_size(self, P: np.ndarray, Q: np.ndarray) -> float:
         a, b = self._compute_coefficients(
             P,
             Q,
@@ -383,14 +377,14 @@ class GraphMatchSolver(BaseEstimator):
         permutation = np.concatenate(
             (np.arange(self.n_seeds), permutation + self.n_seeds)
         )
-        # not_seeds = np.setdiff1d(np.arange(self.n_B), self.partial_match[:, 1])
+        # not_seeds = np.setdiff1d(np.arange(self.n_B), self.seeds[:, 1])
 
         final_permutation = np.empty(self.n_B, dtype=int)
         final_permutation[self.perm_A] = self.perm_B[permutation]
-        # final_permutation[self.partial_match[:, 0]] = self.partial_match[:, 1]
+        # final_permutation[self.seeds[:, 0]] = self.seeds[:, 1]
         # final_permutation[not_seeds] = permutation
-        # permutation += len(self.partial_match[:, 1])  # TODO this is not robust
-        # permutation = np.concatenate((self.partial_match[:, 1], not_seeds[permutation]))
+        # permutation += len(self.seeds[:, 1])  # TODO this is not robust
+        # permutation = np.concatenate((self.seeds[:, 1], not_seeds[permutation]))
         self.permutation_ = final_permutation
 
         score = self.compute_score(permutation)
@@ -406,7 +400,12 @@ class GraphMatchSolver(BaseEstimator):
             return "[Pre-loop]"
 
 
-def _permute_multilayer(adjacency, permutation, rows=True, columns=True):
+def _permute_multilayer(
+    adjacency: MultilayerAdjacency,
+    permutation: np.ndarray,
+    rows: bool = True,
+    columns: bool = True,
+) -> MultilayerAdjacency:
     for layer_index in range(len(adjacency)):
         layer = adjacency[layer_index]
         if rows:
@@ -489,36 +488,12 @@ def _compute_coefficients(
 
 _compute_coefficients_numba = njit(_compute_coefficients)
 
-# TODO: replace use of this function with sinkhorn in POT
-# REF: https://github.com/microsoft/graspologic/blob/dev/graspologic/match/qap.py
-def _doubly_stochastic(P: np.ndarray, tol: float = 1e-3) -> np.ndarray:
-    # Adapted from @btaba implementation
-    # https://github.com/btaba/sinkhorn_knopp
-    # of Sinkhorn-Knopp algorithm
-    # https://projecteuclid.org/euclid.pjm/1102992505
-
-    max_iter = 1000
-    c = 1 / P.sum(axis=0)
-    r = 1 / (P @ c)
-    P_eps = P
-
-    for it in range(max_iter):
-        if (np.abs(P_eps.sum(axis=1) - 1) < tol).all() and (
-            np.abs(P_eps.sum(axis=0) - 1) < tol
-        ).all():
-            # All column/row sums ~= 1 within threshold
-            break
-
-        c = 1 / (r @ P)
-        r = 1 / (P @ c)
-        P_eps = r[:, None] * P * c
-
-    return P_eps
-
 
 def _split_matrix(
-    matrices: np.ndarray, n: int, single_layer: bool = False
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    matrices: MultilayerAdjacency, n: int, single_layer: bool = False
+) -> Tuple[
+    MultilayerAdjacency, MultilayerAdjacency, MultilayerAdjacency, MultilayerAdjacency
+]:
     if single_layer:
         matrices = [matrices]
     n_layers = len(matrices)
