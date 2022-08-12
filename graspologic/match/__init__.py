d59e6d3134eccc819fd30b423be307bfe110426b
bdpedigo <benjamindpedigo@gmail.com>
2022-08-12 12:44:46 -0400

    bye bye to the old code

--- graspologic/match/__init__.py
+++ graspologic/match/__init__.py
@@ -1,8 +1,6 @@
 # Copyright (c) Microsoft Corporation and contributors.
 # Licensed under the MIT License.
 
-from .gmp import GraphMatch
-from .solver import _GraphMatchSolver
 from .wrappers import graph_match
 
-__all__ = ["graph_match", "GraphMatch"]
+__all__ = ["graph_match"]
