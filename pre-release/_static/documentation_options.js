d69762407b474a27ebc854d99ad9e9304ff54c25
daxpryce <daxpryce@users.noreply.github.com>
2022-09-16 20:23:33 +0000

    deploy: 899a4707d0560d5ef4cd3e34c534addfded80c2f

--- pre-release/_static/documentation_options.js
+++ pre-release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.0dev3070066715',
+    VERSION: '2.0.1dev3070390439',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
