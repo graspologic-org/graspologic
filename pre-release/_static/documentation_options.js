b087e41b08c7e91801805588db20a0b9ab3a46d5
bdpedigo <bdpedigo@users.noreply.github.com>
2023-05-16 23:09:51 +0000

    deploy: 44b050054bcf13ceccfb90cd4b0425e6b95d2244

--- pre-release/_static/documentation_options.js
+++ pre-release/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.0dev4534921903',
+    VERSION: '3.0.0dev4997377482',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
