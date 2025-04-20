89c0d83bf4f7dd24df1f6c0a3c4805465531dc8b
Ryan <ryannguyen2102@gmail.com>
2025-04-20 17:41:37 -0400

    draft 1.1

--- graspologic/version.py
+++ graspologic/version.py
@@ -1,6 +1,6 @@
 # This version placeholder will be replaced during package build.
 # Do not commit this file.
-__version__ ="0.0.1.dev842"#
+__version__ ="0.0.1.dev843"#
 #
 def __version() -> str:
     return __version__
